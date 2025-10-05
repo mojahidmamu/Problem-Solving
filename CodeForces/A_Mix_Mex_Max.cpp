f9eed93cb85c307bdf00ee095c0be001a6ab0b35
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 08:18:13 +0600

    added

--- CodeForces/A_Mix_Mex_Max.cpp
+++ CodeForces/A_Mix_Mex_Max.cpp
@@ -4,6 +4,12 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        
+    }
     
     return 0;
 }
