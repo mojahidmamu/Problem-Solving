040b8fd88e5a449f6e01a1d6e7bbed932478ada9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 08:48:20 +0600

    added

--- CodeForces/A_Mix_Mex_Max.cpp
+++ CodeForces/A_Mix_Mex_Max.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
@@ -11,13 +12,37 @@ int main() {
         int N;
         cin >> N;
         int arr[N];
+        vector<int> v;
         for (int i = 0; i < N; i++)
         {
-            cin >> arr[i];
+            if (arr[i] != -1)
+            {
+                v.push_back(arr[i]);
+            }
         }
-        
-        
+
+        bool isGood = false;
+        for (int i = 0; i < N; i++)
+        {
+            if (arr[i] == -1 && arr[i - 1] != -1)
+            {
+                if (abs(arr[i] - arr[i - 1]))
+                {
+                    isGood = true;
+                    break;
+                }
+            }
+        }
+
+        if (isGood)
+        {
+            cout << "YES" << "\n";
+        }
+        else
+        {
+            cout << "NO" << "\n";
+        }
     }
-    
+
     return 0;
 }
