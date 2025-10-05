6d1837c72c389c35f49ac1407a917b55484d7f80
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 08:18:48 +0600

    added

--- CodeForces/A_Mix_Mex_Max.cpp
+++ CodeForces/A_Mix_Mex_Max.cpp
@@ -8,6 +8,14 @@ int main() {
     cin >> T;
     while (T--)
     {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++)
+        {
+            cin >> arr[i];
+        }
+        
         
     }
     
