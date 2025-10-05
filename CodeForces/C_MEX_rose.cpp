ab29526865c2d0fce20fff9931bbb916076d270c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 09:03:42 +0600

    added

--- CodeForces/C_MEX_rose.cpp
+++ CodeForces/C_MEX_rose.cpp
@@ -1,9 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+    }
+
     return 0;
 }
