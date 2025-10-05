b8cb95cdb4451d31be036d1a29d5dc3a9c602615
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 11:49:04 +0600

    added

--- CodeForces/A_Incremental_Subarray.cpp
+++ CodeForces/A_Incremental_Subarray.cpp
@@ -1,9 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n, m;
+        cin >> n >> m;
+        for (int i = 0; i < m; i++)
+        {
+            int a;
+            cin >> a;
+        }
+    }
+
     return 0;
 }
