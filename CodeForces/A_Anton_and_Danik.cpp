4e5d7318d9e3522637d990c2f62c8d057d1c133f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 10:51:18 +0600

    added

--- CodeForces/A_Anton_and_Danik.cpp
+++ CodeForces/A_Anton_and_Danik.cpp
@@ -4,6 +4,10 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    int n;
+    cin >> n;
+    string s ;
+    cin >> s;
     
     return 0;
 }
