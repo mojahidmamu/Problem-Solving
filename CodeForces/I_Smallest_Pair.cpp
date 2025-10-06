357bdf70e72db5bc86c6ec0671785153e71193e0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 08:31:24 +0600

    added

---
+++ CodeForces/I_Smallest_Pair.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        
+    }
+
+    return 0;
+}
