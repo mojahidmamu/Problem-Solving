7fd4a1e4d185d92ebc48421742c283d0c96d6338
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 11:18:46 +0600

    added

--- CodeForces/K_I_Love_strings.cpp
+++ CodeForces/K_I_Love_strings.cpp
@@ -1,9 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        string S;
+        string T;
+        cin >> S >> T;
+
+        string M = "";
+        int n = min(S.size(), T.size());
+
+        for (int i = 0; i < n; i++)
+        {
+            M += S[i];
+            M += T[i];
+        }
+
+        if (S.size() > T.size())
+        {
+            M += S.substr(n);
+        }
+        else if (T.size() > S.size())
+        {
+            M += T.substr(n);
+        }
+
+        cout << M << endl;
+    }
+
     return 0;
 }
