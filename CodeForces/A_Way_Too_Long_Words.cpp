73299a8cd2610ee10f59e36ed5804bd59c5beae2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 10:48:42 +0600

    added

--- CodeForces/A_Way_Too_Long_Words.cpp
+++ CodeForces/A_Way_Too_Long_Words.cpp
@@ -1,9 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        string s;
+        cin >> s;
+        int len = s.size();
+        if (len > 10)
+        {
+            cout << s[0] << len - 2 << s[s.size() - 1] << endl;
+        }
+        else
+        {
+            cout << s << endl;
+        }
+    }
+
     return 0;
 }
