d013fc775c8b78e1ab933a6e1298549e1fc02726
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 08:08:56 +0600

    added

--- CodeForces/C_Print_from_N_to_1.cpp
+++ CodeForces/C_Print_from_N_to_1.cpp
@@ -4,6 +4,11 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+     int n;
+    cin >> n;
+    for (int i = n; i >= 1; i--)
+    {
+        cout << i << endl;
+    }
     return 0;
 }
