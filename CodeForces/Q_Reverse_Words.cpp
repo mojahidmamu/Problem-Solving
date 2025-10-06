5b28269a943eafbaa8f32474456071c1043e2f47
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 11:01:09 +0600

    added

--- CodeForces/Q_Reverse_Words.cpp
+++ CodeForces/Q_Reverse_Words.cpp
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    string s;
+    cin >> s;
+    stringstream ss(s);
+    string word;
+    ss >> word;
+    cout << word << " ";
     return 0;
 }
