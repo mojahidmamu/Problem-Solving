61f00b1177e539e9388324b0e3ef00dda04649e4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 10:57:58 +0600

    added

--- CodeForces/A_Anton_and_Danik.cpp
+++ CodeForces/A_Anton_and_Danik.cpp
@@ -1,13 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     int n;
     cin >> n;
-    string s ;
+    string s;
     cin >> s;
-    
+    int Anton = 0, Danik = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'A')
+        {
+            Anton++;
+        }
+        else
+        {
+            Danik++;
+        }
+    }
+
+    if (Anton > Danik)
+    {
+        cout << "Anton" << endl;
+    }
+    else if (Anton < Danik)
+    {
+        cout << "Danik" << endl;
+    }
+    else if (Anton == Danik)
+    {
+        cout << "Friendship" << endl;
+    }
+
     return 0;
 }
