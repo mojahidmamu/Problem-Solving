34b8f8df66d62820b17e1fe469e8a9354cd15e7f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 08:37:49 +0600

    added

--- CodeForces/I_Smallest_Pair.cpp
+++ CodeForces/I_Smallest_Pair.cpp
@@ -12,11 +12,24 @@ int main()
         int n;
         cin >> n;
         int arr[n];
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i <= n; i++)
         {
             cin >> arr[i];
         }
-        
+
+        int minimum_sum = INT_MAX;
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = i + 1; j <= n; j++)
+            {
+                int sum = arr[i] + arr[j] + j - i;
+                if (sum < minimum_sum)
+                {
+                    minimum_sum = sum;
+                }
+            }
+        }
+        cout << minimum_sum << endl;
     }
 
     return 0;
