39edb167cfdd1e1bade9c932c7b5b4a807e62837
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-07 10:34:00 +0600

    added

--- HackerRank/Replace_word.cpp
+++ HackerRank/Replace_word.cpp
@@ -4,6 +4,19 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        string S, X;
+        cin >> S >> X;
+        while (S.find(X) != string::npos)
+        {
+            S.replace(S.find(X), X.size(), "#");
+        }
+        
+        cout << S << endl;
+    }
     
     return 0;
 }
