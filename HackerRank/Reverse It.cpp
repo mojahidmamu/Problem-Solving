74f2cc7a9a1dbbda835f56e02fe16c13bab3f4c8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-07 11:13:16 +0600

    added

--- HackerRank/Reverse It.cpp
+++ HackerRank/Reverse It.cpp
@@ -1,9 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+
+class Student
+{
+public:
+    string name;
+    int cls;
+    char section;
+    int id;
+};
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int n;
+    cin >> n;
+    Student arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].name >> arr[i].cls >> arr[i].section >> arr[i].id;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].name >> arr[i].cls >> arr[i].section >> arr[i].id;
+    }
+
     return 0;
 }
