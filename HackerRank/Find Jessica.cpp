c2a92d71c27a142381e0f6c06ed9cf169b6feb80
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-07 11:01:52 +0600

    added

--- HackerRank/Find Jessica.cpp
+++ HackerRank/Find Jessica.cpp
@@ -1,9 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    string s;
+    getline(cin, s);
+    stringstream ss(s);
+    string word;
+
+    bool found = false;
+    while (ss >> word)
+    {
+        if (word == "Jessica")
+        {
+            found = true;
+        }
+    }
+
+    if (found == true)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
     return 0;
 }
