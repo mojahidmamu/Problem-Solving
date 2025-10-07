815d70bdc13eeb26fa7051ce119cdc5df067d444
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-07 11:22:37 +0600

    added

--- HackerRank/Sort_it.cpp
+++ HackerRank/Sort_it.cpp
@@ -1,9 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+
+class Student
+{
+public:
+    string name;
+    int cls;
+    char section;
+    int id;
+    int math_marks;
+    int eng_marks;
+};
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int n;
+    cin >> n;
+    Student arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].name >> arr[i].cls >> arr[i].section >> arr[i].id >> arr[i].math_marks >> arr[i].eng_marks;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i].name << " " << arr[i].cls << " " << arr[i].section << " " << arr[i].id << arr[i].math_marks << " " << arr[i].eng_marks << endl;
+    }
+
     return 0;
 }
