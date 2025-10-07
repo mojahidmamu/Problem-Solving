be3a8966e6f7cd530258477e3a4d88f9f4a62b43
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-07 11:39:08 +0600

    added

--- HackerRank/Sort_it.cpp
+++ HackerRank/Sort_it.cpp
@@ -12,6 +12,29 @@ public:
     int eng_marks;
 };
 
+bool cmp(Student l, Student r)
+{
+    if (l.math_marks + l.eng_marks > r.math_marks + r.eng_marks)
+    {
+        return true;
+    }
+    else if (l.math_marks + l.eng_marks == r.math_marks + r.eng_marks)
+    {
+        if (l.id < r.id)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -24,10 +47,12 @@ int main()
     {
         cin >> arr[i].name >> arr[i].cls >> arr[i].section >> arr[i].id >> arr[i].math_marks >> arr[i].eng_marks;
     }
+    
+    sort(arr, arr + n, cmp);
 
     for (int i = 0; i < n; i++)
     {
-        cout << arr[i].name << " " << arr[i].cls << " " << arr[i].section << " " << arr[i].id << arr[i].math_marks << " " << arr[i].eng_marks << endl;
+        cout << arr[i].name << " " << arr[i].cls << " " << arr[i].section << " " << arr[i].id << " " << arr[i].math_marks << " " << arr[i].eng_marks << endl;
     }
 
     return 0;
