27228cd857476d50d45a39dd0d96dbc1474e95bf
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-10 09:09:37 +0600

    added

--- HackerRank/Sorted.cpp
+++ HackerRank/Sorted.cpp
@@ -1,33 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++)
+    int t;
+    cin >> t;
+    while (t--)
     {
-        cin >> arr[i];
-    }
-
-    bool isSorted = false;
-    for (int i = 1; i <= n; i++)
-    {
-        if (arr[i] > arr[i - 1])
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
         {
-             isSorted = true;
+            cin >> arr[i];
         }
-    }
 
-    if (isSorted)
-    {
-        cout << "YES" << endl;
-    }
-    else
-    {
-        cout << "NO" << endl;
+        bool isSorted = true;
+        for (int i = 0; i < n -1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                isSorted = false;
+                break; 
+            }
+        }
+
+        if (isSorted)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
 
     return 0;
