71502de054316b5e7cd989d3cb9ee1a41646120e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-10 08:58:08 +0600

    added

--- HackerRank/Insert it.cpp
+++ HackerRank/Insert it.cpp
@@ -7,18 +7,25 @@ int main()
     // --- your code here ---
     int n;
     cin >> n;
-    int arr1[n];
+    vector<int> arr1(n);
     for (int i = 0; i < n; i++)
     {
         cin >> arr1[i];
     }
     int m;
     cin >> m;
-    int arr2[m];
+    vector<int> arr2(m);
     for (int i = 0; i < m; i++)
     {
         cin >> arr2[i];
     }
-    
+    int index ;
+    cin >> index;
+     
+    arr1.insert(arr1.begin() + index, arr2.begin(), arr2.end());
+    for(int val : arr1)
+    {
+        cout << val << " ";
+    }
     return 0;
 }
