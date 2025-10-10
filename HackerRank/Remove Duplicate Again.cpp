65a4150de75f21e70f4f1966dd5ebb0ffd46e9c8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-10 15:16:09 +0600

    added

--- HackerRank/Remove Duplicate Again.cpp
+++ HackerRank/Remove Duplicate Again.cpp
@@ -1,9 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    else
+    {
+        tail->next = newNode;
+        tail = newNode;
+    }
+};
+
+void print_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void remove_duplicates(Node *&head)
+{
+    Node *curr = head;
+    while (curr != NULL && curr->next != NULL)
+    {
+        if (curr->val == curr->next->val)
+        {
+            Node *temp = curr->next;
+            curr->next = curr->next->next;
+            delete temp;
+        }
+        else
+        {
+            curr = curr->next;
+        }
+    }
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while (true)
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        else
+        {
+            insert_at_tail(head, tail, val);
+        }
+    }
+
+    remove_duplicates(head);
+    print_list(head);
     return 0;
 }
