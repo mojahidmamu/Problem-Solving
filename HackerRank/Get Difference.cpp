ca426f19854a660c56f946c68e9eeb1ee433ce4b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-10 09:18:52 +0600

    added

--- HackerRank/Get Difference.cpp
+++ HackerRank/Get Difference.cpp
@@ -1,9 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int &val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    else
+    {
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    Node *head = new NULL;
+    Node *tail = new NULL;
+
+    while (true)
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        else
+        {
+            insert_at_tail(head, tail, val);
+        }
+    }
+
     return 0;
 }
