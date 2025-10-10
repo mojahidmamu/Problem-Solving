7a4c4ddbbf76d1959a360113cdb86cd582bcb8ee
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-10 15:36:44 +0600

    added

--- HackerRank/Palindrome.cpp
+++ HackerRank/Palindrome.cpp
@@ -1,9 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    else
+    {
+        tail->next = newNode;
+        tail = newNode;
+    }
+};
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (cin >> val && val != -1)
+    {
+        insert_at_tail(head, tail, val);
+    }
     return 0;
 }
