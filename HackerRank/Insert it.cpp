a13d1e1f1d5ad3e9f31bd3198bdf88fefca986ae
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-10 08:52:06 +0600

    added

--- HackerRank/Insert it.cpp
+++ HackerRank/Insert it.cpp
@@ -1,9 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    int n;
+    cin >> n;
+    int arr1[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr1[i];
+    }
+    int m;
+    cin >> m;
+    int arr2[m];
+    for (int i = 0; i < m; i++)
+    {
+        cin >> arr2[i];
+    }
     
     return 0;
 }
