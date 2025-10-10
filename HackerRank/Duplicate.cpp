e3c354a9f957a7775ec97ab96b6bb43ba72cc3e7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-10 08:45:11 +0600

    added

--- HackerRank/Duplicate.cpp
+++ HackerRank/Duplicate.cpp
@@ -1,12 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     int n;
     cin >> n;
     int arr[n];
-    for
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    bool isDuplicate = false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                isDuplicate = true;
+                break;
+            }
+        }
+        // if (isDuplicate)
+        // {
+        //     break;
+        // }
+    }
+    if (isDuplicate)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
     return 0;
 }
