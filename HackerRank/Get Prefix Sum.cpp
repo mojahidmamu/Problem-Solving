76ebd428712c0259b0795ea1213ee0f1ad6bdae6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-10 08:48:41 +0600

    added

--- HackerRank/Get Prefix Sum.cpp
+++ HackerRank/Get Prefix Sum.cpp
@@ -4,6 +4,23 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int prefixSum[n];
+    prefixSum[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        prefixSum[i] = prefixSum[i - 1] + arr[i];
+    }
+    reverse(prefixSum, prefixSum + n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << prefixSum[i] << " ";
+    }
     return 0;
 }
