5883fcee3f4fccfd0fa7a1e665d5a450e8b3940e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-12 09:33:32 +0600

    added

--- HackerRank/Remember Previous Queries.cpp
+++ HackerRank/Remember Previous Queries.cpp
@@ -1,9 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int X, V;
+        cin >> X >> V;
+        
+    }
     
     return 0;
 }
