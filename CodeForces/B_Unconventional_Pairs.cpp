60a6e2392bc27e43d89af3fc61e07c748406300f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-14 10:49:09 +0600

    added

--- CodeForces/B_Unconventional_Pairs.cpp
+++ CodeForces/B_Unconventional_Pairs.cpp
@@ -1,9 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        cin >> a;
+        int arr[a];
+        for (int i = 0; i < a; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr, arr + a);
+
+        int maxDiff = 0;
+        for (int i = 0; i < a - 1; i += 2)
+        {
+            int Diff = abs(arr[i] - arr[i + 1]);
+            maxDiff = max(maxDiff, Diff);
+        }
+
+        cout << maxDiff << "\n";
+    }
     return 0;
 }
