02bdd0b217866a6969e9de2edd78d544467977ca
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-14 08:53:43 +0600

    added

--- CodeForces/A_El_fucho.cpp
+++ CodeForces/A_El_fucho.cpp
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int a;
+        cin >> a;
+        
+    }
     return 0;
 }
