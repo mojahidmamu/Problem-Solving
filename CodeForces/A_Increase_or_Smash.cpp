530216fa8dbdd5d4754bb424fc33333b0cfd8fd4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-14 09:50:05 +0600

    added

--- CodeForces/A_Increase_or_Smash.cpp
+++ CodeForces/A_Increase_or_Smash.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        cin >> a;
+        int arr[a];
+        for (int i = 0; i < a; i++)
+        {
+            cin >> arr[i];
+        }
+    }
     return 0;
 }
