b0213ecb90741532f442b829066496990f3c55c3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-14 10:51:50 +0600

    added

---
+++ CodeForces/A_Sublime_Sequence.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b;
+        cin >> a >> b;
+         
+    }
+    return 0;
+}
