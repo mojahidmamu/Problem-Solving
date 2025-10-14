b1a0be9ffbf4a9ae5d552181f01c73105b4d0c9b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-14 11:21:09 +0600

    added

--- CodeForces/A_Shortest_Increasing_Path.cpp
+++ CodeForces/A_Shortest_Increasing_Path.cpp
@@ -11,7 +11,55 @@ int main()
     {
         int x, y;
         cin >> x >> y;
-        
+
+        // current position:
+        long long cx = 0, cy = 0;
+        long long step = 1;
+        bool isPossible = true;
+        int ans = 0;
+        while (cx < x || cy < y)
+        {
+            if (ans % 2 == 0)
+            {
+                if (cx + step <= x)
+                {
+                    cx += step;
+                    ans++;
+                }
+                else
+                {
+                    isPossible = false;
+                    break;
+                }
+            }
+            else if (ans % 2 == 1)
+            {
+                if (cy + step <= y)
+                {
+                    cy += step;
+                    ans++;
+                } 
+                else
+                {
+                    isPossible = false;
+                    break;
+                }
+            }
+            else 
+            {
+                isPossible = false;
+                break;
+            }
+        }
+
+        if (isPossible)
+        {
+            cout << ans << "\n";
+        }
+        else
+        {
+            cout << -1 << "\n";
+        }
     }
     return 0;
 }
