92f14fcf29f44c687fb6a2ef55c30a6b7042d481
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-15 09:00:43 +0600

    added

--- CodeForces/A_Candies_for_Nephews.cpp
+++ CodeForces/A_Candies_for_Nephews.cpp
@@ -9,7 +9,18 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
-        
+        if (n % 3 == 0)
+        {
+            cout << 0 << "\n";
+        }
+        else if (n % 3 == 1)
+        {
+            cout << 2 << "\n";
+        }
+        else
+        {
+            cout << 1 << "\n";
+        }
     }
     return 0;
 }
