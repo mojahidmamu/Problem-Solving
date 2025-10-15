c23dd39fc3209777b9d312e8cc956a023ed5be41
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-15 08:41:48 +0600

    added

--- CodeForces/A_Equal_Occurrences.cpp
+++ CodeForces/A_Equal_Occurrences.cpp
@@ -13,8 +13,24 @@ int main() {
         for (int i = 0; i < n; i++) {
             cin >> arr[i];
         }
+
         int length = 0;
+        int freq[n + 1];
+        // count frequency of each number: 1 to n
+        for (int i = 1; i <= n; i++) {
+            freq[i] = 0;
+        }
+        for (int i = 0; i < n; i++) {
+            freq[arr[i]]++;
+        }
         
+
+
+
+
+
+        cout << length << "\n";
+
     }
     return 0;
 }
