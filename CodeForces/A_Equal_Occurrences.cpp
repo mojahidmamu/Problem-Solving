0cbd3c75645d1c25a00f3aac85875c6ce77a58b1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-15 08:46:49 +0600

    added

--- CodeForces/A_Equal_Occurrences.cpp
+++ CodeForces/A_Equal_Occurrences.cpp
@@ -1,36 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     int t;
     cin >> t;
-    while (t--) {   
+    while (t--)
+    {
         int n;
         cin >> n;
         int arr[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             cin >> arr[i];
         }
 
         int length = 0;
-        int freq[n + 1];
+        map<int, int> freq;
         // count frequency of each number: 1 to n
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++)
+        {
             freq[i] = 0;
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             freq[arr[i]]++;
         }
-        
 
+        int max_freq = 0;
+        for (auto &p : freq)
+        {
+            max_freq = max(max_freq, p.second);
+        }
 
-
-
+        for (int f = 1; f < -max_freq; f++)
+        {
+            int cnt = 0;
+            for (auto &p : freq)
+            {
+                if (p.second >= f)
+                {
+                    cnt++;
+                }
+            }
+            length = max(length, cnt * f);
+        }
 
         cout << length << "\n";
-
     }
     return 0;
 }
