a803a75d7f9e3b31e35a0eb2e12b339ebc9d7b9c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-15 09:08:50 +0600

    added

--- CodeForces/A_Cut_the_Array.cpp
+++ CodeForces/A_Cut_the_Array.cpp
@@ -7,6 +7,12 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
         
     }
     return 0;
