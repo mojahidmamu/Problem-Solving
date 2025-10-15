ecd46d1faed0f550a31ab8f457feab92623af19b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-15 10:01:47 +0600

    added

--- CodeForces/A_Shift_Sort.cpp
+++ CodeForces/A_Shift_Sort.cpp
@@ -4,6 +4,17 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        
+    }
     return 0;
 }
