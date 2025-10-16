7aad51b685d267a85d09a5170dae9779e11dba85
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-16 11:01:00 +0600

    added

--- CodeForces/A_Collatz_Conjecture.cpp
+++ CodeForces/A_Collatz_Conjecture.cpp
@@ -1,9 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int k, x;
+        cin >> k >> x;
+        while (k--)
+        {
+            if ((x - 1) % 3 == 0 && ((x - 1) / 3) % 2 == 1)
+            {
+                x = (x - 1) / 3;
+            }
+            else
+            {
+                x *= 2;
+            }
+        }
+        cout << x << "\n";
+    }
     return 0;
 }
