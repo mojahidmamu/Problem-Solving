b4d69f209afc1fb9af28c13c66453f7a28f556b3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-17 08:58:57 +0600

    added

--- CodeForces/B_Tournament.cpp
+++ CodeForces/B_Tournament.cpp
@@ -1,9 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, j, k;
+        cin >> n >> j >> k;
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> a[i];
+        }
+
+        bool possible = true;
+        
+
+
+
+        if (possible)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
     return 0;
 }
