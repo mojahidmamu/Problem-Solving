662791c46202b6a42a0673028c848bf9e0b61749
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-17 22:12:38 +0600

    added

--- CodeForces/Contests_1/H_Beautiful_Problem.cpp
+++ CodeForces/Contests_1/H_Beautiful_Problem.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    int t;
+    cin >> t;
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    
     
     return 0;
 }
