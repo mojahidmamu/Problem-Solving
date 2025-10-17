a9b7410d01f0ac8c746d8bf86d07eb64ad9ae86b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-17 21:32:54 +0600

    added

--- CodeForces/Contests_1/E_Beautiful_Palindromes.cpp
+++ CodeForces/Contests_1/E_Beautiful_Palindromes.cpp
@@ -1,41 +1,74 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     // --- your code here ---
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n, k;
+//         cin >> n >> k;
+//         vector<int> a(n);
+//         for (int i = 0; i < n; i++)
+//         {
+//             cin >> a[i];
+//         }
+
+//         vector<int> ans;
+//         int x = 1;
+//         while (ans.size() < k)
+//         {
+//             if (x != a.back())
+//             {
+//                 ans.push_back(x);
+//             }
+//             x++;
+
+//             if (x > n)
+//             {
+//                 x = 1;  
+//             }
+//         }
+
+//         for (int i = 0; i < k; i++)
+//         {
+//             cout << ans[i] << (i == k - 1 ? "\n" : " ");
+//         }
+//     }
+
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    // --- your code here ---
+
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         int n, k;
         cin >> n >> k;
         vector<int> a(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
+        for (int i = 0; i < n; i++) cin >> a[i];
 
         vector<int> ans;
         int x = 1;
-        while (ans.size() < k)
-        {
-            if (x != a.back())
-            {
+        while (ans.size() < k) {
+            if (x != a.back()) {
                 ans.push_back(x);
-                x++;
             }
-            if (x > n)
-            {
-                x = 1;  
-            }
+            x++;
+            if (x > n) x = 1; // wrap around
         }
 
         for (int i = 0; i < k; i++)
-        {
             cout << ans[i] << (i == k - 1 ? "\n" : " ");
-        }
     }
 
     return 0;
