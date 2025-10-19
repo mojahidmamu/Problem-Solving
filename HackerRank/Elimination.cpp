339b2c90612f3d1ef499bac23924a9d63bc9b7fe
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-19 10:31:04 +0600

    added

---
+++ HackerRank/Elimination.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+    }
+    
+    return 0;
+}
