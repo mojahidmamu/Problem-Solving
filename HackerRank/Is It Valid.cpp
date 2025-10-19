e3a01982eb65460f96d3c5ddb9736cab877569d7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-19 09:09:32 +0600

    added

--- HackerRank/Is It Valid.cpp
+++ HackerRank/Is It Valid.cpp
@@ -1,6 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+bool isValid(string &s)
+{
+    stack<char> st;
+    for (char c : s)
+    {
+        if (c == '0')
+        {
+            if (!st.empty() && st.top() == '1')
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push('0');
+            }
+        }
+        else if (c == '1')
+        {
+            if (!st.empty() && st.top() == '0')
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push('1');
+            }
+        }
+    }
+    return st.empty();
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
@@ -10,7 +42,15 @@ int main() {
     {
         string s;
         cin >> s;
+        if (isValid(s))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
-    
+
     return 0;
 }
