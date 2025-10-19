a97e466de9c40d2a04a8f31ea94e1aa81bd12276
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-19 11:25:00 +0600

    added

--- HackerRank/Elimination.cpp
+++ HackerRank/Elimination.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+
+bool isElimination(string s)
+{
+    stack<char> st;
+    for (char ch : s)
+    {
+        if (ch == '1' && !st.empty() && st.top() == '0')
+        {
+            st.pop();
+        }
+        else
+        {
+            st.push(ch);
+        }
+    }
+    return st.empty();
+}
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
@@ -10,7 +28,15 @@ int main() {
     {
         string s;
         cin >> s;
+        if (isElimination(s))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
-    
+
     return 0;
 }
