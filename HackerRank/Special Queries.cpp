833c73e6b51ec83dcf3121c0a1c02f19e8afe3e1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-19 10:28:55 +0600

    added

--- HackerRank/Special Queries.cpp
+++ HackerRank/Special Queries.cpp
@@ -1,27 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     int t;
     cin >> t;
+    stack<string> st;
     while (t--)
     {
         int command;
         cin >> command;
-        if (command == '0')
+        if (command == 0)
         {
             string Name;
             cin >> Name;
+            st.push(Name);
         }
-        else if (command == '1')
+        else if (command == 1)
         {
-            
+            if (st.empty())
+            {
+                cout << "Invalid" << endl;
+            }
+            else
+            {
+                cout << st.top() << endl;
+                st.pop();
+            }
         }
-        
-        
     }
-    
+
     return 0;
 }
