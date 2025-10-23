db05e7ddad5914fbe9e288b753c325d1beff45c0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-23 12:18:01 +0600

    added

--- HackerRank/Level Nodes.cpp
+++ HackerRank/Level Nodes.cpp
@@ -48,12 +48,51 @@ Node *input_binary_tree()
     return root;
 }
 
+void order_level(Node *root, int target_level)
+{
+    if (!root)
+    {
+        cout << "Invalid" << endl;
+        return;
+    }
+
+    queue<pair<Node *, int>> q;
+    q.push({root, 0});
+    bool found = false;
+
+    while (!q.empty())
+    {
+        Node *current = q.front().first;
+        int level = q.front().second;
+
+        if (level == target_level)
+        {
+            cout << current->val << " ";
+            found = true;
+        }
+        else if (level > target_level)
+        {
+            break;
+        }
+        if (current->left)
+        {
+            q.push({current->left, level + 1});
+        }
+        if (current->right)
+        {
+            q.push({current->right, level + 1});
+        }
+    }
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     Node *root = input_binary_tree();
-
+    int x;
+    cin >> x;
+    order_level(root, x);
     return 0;
 }
