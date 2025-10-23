bf5b1d9c3a17aa363051ebf88f4a83a9fba104fc
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-23 11:44:14 +0600

    added

--- HackerRank/Leaf Nodes.cpp
+++ HackerRank/Leaf Nodes.cpp
@@ -48,12 +48,33 @@ Node *input_binary_tree()
     return root;
 }
 
+void count_leaf_nodes(Node *root, vector<int> &v)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    count_leaf_nodes(root->left, v);
+    count_leaf_nodes(root->right, v);
+    if (root->left == NULL && root->right == NULL)
+    {
+        v.push_back(root->val);
+    }
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     Node *root = input_binary_tree();
-    
+    vector<int> leaf_nodes;
+    count_leaf_nodes(root, leaf_nodes);
+    sort(leaf_nodes.rbegin(), leaf_nodes.rend());
+    for (int val : leaf_nodes)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
     return 0;
 }
