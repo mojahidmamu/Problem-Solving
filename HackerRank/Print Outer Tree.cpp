1a7040c73950b9bb2d502a65b41963a1e3237a29
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-23 12:19:19 +0600

    added

---
+++ HackerRank/Print Outer Tree.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+// input :
+Node *input_binary_tree()
+{
+    int val;
+    cin >> val;
+
+    if (val == -1)
+        return NULL;
+
+    Node *root = new Node(val);
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *current = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        if (l != -1)
+        {
+            current->left = new Node(l);
+            q.push(current->left);
+        }
+        if (r != -1)
+        {
+            current->right = new Node(r);
+            q.push(current->right);
+        }
+    }
+    return root;
+}
+
+// max-height:
+int max_height(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        return 0;
+    }
+    int l = max_height(root->left);
+    int r = max_height(root->right);
+    int maximum = max(l, r);
+    return maximum + 1;
+}
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    Node *root = input_binary_tree();
+    
+
+    return 0;
+}
