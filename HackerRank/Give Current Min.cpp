74cfbf67825cc571529cdc22f3d4cdcac87fda95
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-24 12:02:32 +0600

    added

--- HackerRank/Give Current Min.cpp
+++ HackerRank/Give Current Min.cpp
@@ -1,9 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+};
+
+bool Compare(Student a, Student b)
+{
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    priority_queue<Student, vector<Student>, Compare> pq;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        Student s;
+        cin >> s.name >> s.roll >> s.marks;
+        pq.push({s.name, s.roll , s.marks});
+    }
+
+    int Q;
+    cin >> Q;
+    while (Q--)
+    {
+        int cmd;
+        cin >> cmd;
+        if (cmd == 0)
+        {
+            
+        }
+        else if (cmd == 1)
+        {
+            /* code */
+        }
+        else if (cmd == 2)
+        {
+            /* code */
+        }
+        
+        
+        
+    }
     
+
     return 0;
 }
