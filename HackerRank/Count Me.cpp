e48dc5fa8831b59c01ac5325f6478afc30c0c545
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-24 11:37:36 +0600

    added

--- HackerRank/Count Me.cpp
+++ HackerRank/Count Me.cpp
@@ -1,9 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        string word;
+        map<string, int> count; 
+
+        string maxWord;
+        int maxCount = 0;
+        while (ss >> word)
+        {
+            count[word]++;
+            if (count[word] > maxCount)
+            {
+                maxCount = count[word];
+                maxWord = word;
+            }
+        }
+        cout << maxWord << " " << maxCount << endl;
+    }
+
     return 0;
 }
