49be2db6eb928601f60b23a3efa0f34aa9757c6f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-25 19:21:30 +0600

    added

--- CodeForces/A_Vadim_s_Collection.cpp
+++ CodeForces/A_Vadim_s_Collection.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
@@ -8,8 +9,29 @@ int main() {
     cin >> t;
     while (t--)
     {
-        
+        string s;
+        cin >> s;
+        vector<int> cnt(10, 0);
+        for (char c : s)
+        {
+            cnt[c - '0']++;
+        }
+        string ans;
+        for (int i = 0; i < 10; i++)
+        {
+            int need = 9 - i;
+            for (int d = need; d <= 9; d++)
+            {
+                if (cnt[d] > 0)
+                {
+                    ans.push_back('0' + d);
+                    cnt[d]--;
+                    break;
+                }
+            }
+        }
+        cout << ans << endl;
     }
-    
+
     return 0;
 }
