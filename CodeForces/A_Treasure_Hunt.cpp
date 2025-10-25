7850e25a979b311ef2ea39c592037348ebd10b3a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-25 19:48:12 +0600

    added

---
+++ CodeForces/A_Treasure_Hunt.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long x, y, a;
+        cin >> x >> y >> a;
+        
+    }
+    
+    return 0;
+}
