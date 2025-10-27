139b5cbae0e77521c77d0f87bf2c8a2733a19d6c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-27 07:54:20 +0600

    added

--- HackerRank/Connected or Not.cpp
+++ HackerRank/Connected or Not.cpp
@@ -1,9 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+
+vector<int> adj_list[1005];
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int N, E;
+    cin >> N >> E;
+    while (E--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+    }
+
+    int Q;
+    cin >> Q;
+    while (Q--)
+    {
+        int A, B;
+        cin >> A >> B;
+    }
+
     return 0;
 }
