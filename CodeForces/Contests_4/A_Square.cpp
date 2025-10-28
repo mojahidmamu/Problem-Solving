4dd3b32765be1a9e37bd26d1d923435755aee2ea
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-28 20:38:08 +0600

    added

---
+++ CodeForces/Contests_4/A_Square.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        bool isSquare = false;
+        if (a == b && b == c && c == d)
+        {
+            isSquare = true;
+        }
+
+        if (isSquare)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
