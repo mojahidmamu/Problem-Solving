e4a5b99421b7e2cde121f1b895a0f63142178913
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-28 09:35:27 +0600

    added

---
+++ CodeForces/A_Draw_a_Square.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        bool isSquare = false;
+
+        if (isSquare)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
