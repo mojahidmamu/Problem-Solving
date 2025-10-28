eb73a823caa8fb91efc8ad819d1fde7997d82ad8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-28 11:03:07 +0600

    added

--- CodeForces/A_New_World_New_Me_New_Array.cpp
+++ CodeForces/A_New_World_New_Me_New_Array.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
@@ -10,8 +11,28 @@ int main() {
     {
         int n, k, p;
         cin >> n >> k >> p;
-        
+
+        if (k < -n * p || k > n * p)
+        {
+            cout << -1 << endl;
+        }
+        else if (k == 0)
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            int ops = (abs(k) + p - 1) / p;
+            if (ops > n)
+            {
+                cout << -1 << endl;
+            }
+            else
+            {
+                cout << ops << endl;
+            }
+        }
     }
-    
+
     return 0;
 }
