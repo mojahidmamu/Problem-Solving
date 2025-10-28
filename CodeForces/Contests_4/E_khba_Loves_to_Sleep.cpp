0a87519f77873294b9fe33021a5b54cc0b01d3b8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-28 22:44:51 +0600

    added

--- CodeForces/Contests_4/E_khba_Loves_to_Sleep.cpp
+++ CodeForces/Contests_4/E_khba_Loves_to_Sleep.cpp
@@ -1,9 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k, x;
+        cin >> n >> k >> x;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }   
+        
+    }
+
     return 0;
 }
--- CodeForces/Contests_4/G_Mukhammadali_and_the_Smooth_Array.cpp
+++ CodeForces/Contests_4/G_Mukhammadali_and_the_Smooth_Array.cpp
@@ -19,9 +19,43 @@ int main()
 
         vector<long long> cost(n);
         for (int i = 0; i < n; i++)
-        {   
+        {
             cin >> cost[i];
         }
+
+        vector<long long> value = arr;
+        sort(value.begin(), value.end());
+        value.erase(unique(value.begin(), value.end()), value.end());
+
+        int m = value.size();
+        vector<long long> dp(m, LLONG_MAX);
+
+        for (int j = 0; j < m; j++)
+        {
+            if (arr[0] == value[j])
+            {
+                dp[j] = 0;
+            }
+            else
+            {
+                dp[j] = cost[0];
+            }
+        }
+
+        for (int i = 1; i < n; i++)
+        {
+            vector<long long> new_dp(m, LLONG_MAX);
+            long long prefix_min = LLONG_MAX;
+            for (int j = 0; j < m; j++)
+            {
+                prefix_min = min(prefix_min, dp[j]);
+                long long change_cost = (arr[i] == value[j] ? 0 : cost[i]);
+                new_dp[j] = prefix_min + change_cost;
+            }
+            dp = new_dp;
+        }
+
+        cout << *min_element(dp.begin(), dp.end()) << endl;
     }
 
     return 0;
