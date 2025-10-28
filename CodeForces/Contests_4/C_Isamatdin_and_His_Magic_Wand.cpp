d1f56d2100a3349ce339771a3b775ab91b3dc470
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-28 20:53:23 +0600

    added

--- CodeForces/Contests_4/C_Isamatdin_and_His_Magic_Wand.cpp
+++ CodeForces/Contests_4/C_Isamatdin_and_His_Magic_Wand.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
@@ -10,14 +11,42 @@ int main() {
     {
         int n;
         cin >> n;
-        long long int arr[n];
+        vector< long long> arr(n);
         for (int i = 0; i < n; i++)
-        {   
+        {
             cin >> arr[i];
         }
 
-        
+        vector<long long> odd, even;
+        for (auto val : arr)
+        {
+            if (val % 2 == 0)
+            {
+                even.push_back(val);
+            }
+            else
+            {
+                odd.push_back(val);
+            }
+        }
+
+        sort(odd.begin(), odd.end());
+        sort(even.begin(), even.end());
+
+        int i_odd = 0, i_even = 0;
+        for (auto val : arr)
+        {
+            if (val % 2 == 0)
+            {
+                cout << even[i_even++] <<" ";
+            }
+            else
+            {
+                cout << odd[i_odd++] << " ";
+            }
+        }
+        cout << endl;
     }
-    
+
     return 0;
 }
