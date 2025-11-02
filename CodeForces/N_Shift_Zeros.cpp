a8b2842c1631ab5a7b271ed9ef1405b60d153f10
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-02 09:16:56 +0600

    added

--- CodeForces/N_Shift_Zeros.cpp
+++ CodeForces/N_Shift_Zeros.cpp
@@ -1,5 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+void shiftRight(int arr[], int n)
+{
+    int pos = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            arr[pos] = arr[i];
+            pos++;
+        }
+    }
+
+    while (pos < n)
+    {
+        arr[pos] = 0;
+        pos++;
+    }
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -12,6 +32,14 @@ int main()
     {
         cin >> arr[i];
     }
+
+    shiftRight(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
