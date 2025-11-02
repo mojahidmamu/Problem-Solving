c7092cd301463ca784d3be3c704323e7ac04bf09
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-02 09:20:16 +0600

    added

---
+++ CodeForces/P_Log_2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    long long N;
+    cin >> N;
+    int ans = (int)log2(N);
+    cout << ans << endl;
+
+    return 0;
+}
