a34ee4fc2940b2f86473b5c5ae9e53eef383ab6f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-02 08:13:40 +0600

    added

---
+++ CodeForces/B_Memo_and_Momo.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    long long a, b, k;
+    cin >> a >> b >> k;
+    if (a % k == 0 && b % k == 0)
+    {
+        cout << "Both" << endl;
+    }
+    else if (a % k == 0)
+    {
+        cout << "Memo" << endl;
+    }
+    else if (a % k == 0)
+    {
+        cout << "Momo" << endl;
+    }
+    else
+    {
+        cout << "No One" << endl;
+    }
+
+    return 0;
+}
