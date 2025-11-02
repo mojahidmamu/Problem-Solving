07f9e7569fc8298146ec6401b50aa2fc15b36f0e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-02 07:26:04 +0600

    added

--- CodeForces/A_Beautiful_Matrix.cpp
+++ CodeForces/A_Beautiful_Matrix.cpp
@@ -1,9 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+
+    int x, row, col;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cin >> x;
+            if (x == 1)
+            {
+                row = i;
+                col = j;
+            }
+        }
+    }
+
+    cout << abs(row - 3) + abs(col - 3);
     return 0;
 }
