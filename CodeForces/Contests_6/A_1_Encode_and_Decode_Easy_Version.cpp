9992e147db52d27a2fc06f670632ed0893163bda
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-04 07:00:18 +0600

    added

--- CodeForces/Contests_6/A_1_Encode_and_Decode_Easy_Version.cpp
+++ CodeForces/Contests_6/A_1_Encode_and_Decode_Easy_Version.cpp
@@ -4,6 +4,14 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    string s;
+    cin >> s;
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
     
     return 0;
 }
