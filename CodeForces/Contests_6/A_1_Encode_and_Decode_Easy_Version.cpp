eddfd0d89d1b524d9bac87b3e56641a3c77a92a5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-04 07:09:02 +0600

    added

--- CodeForces/Contests_6/A_1_Encode_and_Decode_Easy_Version.cpp
+++ CodeForces/Contests_6/A_1_Encode_and_Decode_Easy_Version.cpp
@@ -1,17 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     string s;
     cin >> s;
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+
+    if (s == "first")
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        string encoded = "";
+        for (int i = 0; i < n; i++)
+        {
+            encoded += ('a' + arr[i] - 1);
+        }
+        cout << encoded << "\n";
     }
-    
+    else if (s == "second")
+    {
+        string encoded;
+        cin >> encoded;
+        int n = encoded.size();
+        cout << n << " ";
+
+        for (char c : encoded)
+        {
+            cout << (c - 'a' + 1) << " ";
+        }
+        cout << "\n";
+    }
+
     return 0;
 }
