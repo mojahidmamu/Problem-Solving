f50922efa77e48d2321d08e7ce1ae00032630e7a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-05 18:39:12 +0600

    added

--- CodeForces/C_Hard_Problem.cpp
+++ CodeForces/C_Hard_Problem.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int m, a, b, c;
+        cin >> m >> a >> b >> c;
+    }
     return 0;
 }
