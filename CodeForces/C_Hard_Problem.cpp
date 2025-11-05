30ab1471d2fe613a5f5398e865c1b6fa77bb9d5c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-05 18:53:22 +0600

    added

--- CodeForces/C_Hard_Problem.cpp
+++ CodeForces/C_Hard_Problem.cpp
@@ -11,6 +11,12 @@ int main()
     {
         int m, a, b, c;
         cin >> m >> a >> b >> c;
+        int row1 = min(a, m);
+        int row2 = min(b, m);
+        int total = row1 + row2;
+        int raw_remaining = 2 * m - total;
+        int result = total + min(c, raw_remaining);
+        cout << result << "\n";
     }
     return 0;
 }
