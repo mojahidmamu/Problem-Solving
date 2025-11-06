87260cf5daffb459933959fb082b0549291564ad
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-06 20:43:12 +0600

    sanda

---
+++ CodeForces/Contests_7/A_Sequence_Game.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<long long> arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        long long x;
+        cin >> x;
+        bool possible = false;
+
+        if (possible)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
