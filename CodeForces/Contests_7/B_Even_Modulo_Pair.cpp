0bb2023e1c91dcbccf03babb29f39685d49a38ce
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-06 20:59:07 +0600

    sanda

--- CodeForces/Contests_7/B_Even_Modulo_Pair.cpp
+++ CodeForces/Contests_7/B_Even_Modulo_Pair.cpp
@@ -1,9 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        bool has_even = false;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if ((arr[i + 1] % arr[i]) % 2 == 0)
+            {
+                cout << arr[i] << " " << arr[i + 1] << endl;
+                has_even = true;
+                break;
+            }
+        }
+
+
+        if (!has_even)
+        {
+            cout << "-1" << endl;
+        }
+        
+    }
     return 0;
 }
