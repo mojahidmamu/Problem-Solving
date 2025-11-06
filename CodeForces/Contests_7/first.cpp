1e43196d792efcda1f1e41fa8f6611db27268619
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-06 19:40:54 +0600

    sanda

---
+++ CodeForces/Contests_7/first.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    cout << "Hello, World!" << endl;
+    // --- your code here ---
+    return 0;
+}
