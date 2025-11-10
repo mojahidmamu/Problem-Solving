5a48a57d47b5cdd8eeb364f958c942683765a496
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-10 21:28:33 +0600

    clousure

---
+++ CodeForces/Contests_6/C_Monopati.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        vector<int> arr2(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr2[i];
+        }
+    }
+    return 0;
+}
