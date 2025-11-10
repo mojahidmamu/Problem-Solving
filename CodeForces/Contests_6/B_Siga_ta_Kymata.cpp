c8ef62f354fc48718eef3ee5f586d0cfa5a812ef
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-10 21:24:31 +0600

    clousure

--- CodeForces/Contests_6/B_Siga_ta_Kymata.cpp
+++ CodeForces/Contests_6/B_Siga_ta_Kymata.cpp
@@ -1,9 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        string s;
+        cin >> s;
+        
+    }
     return 0;
 }
