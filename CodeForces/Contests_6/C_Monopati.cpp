0bcc3a41d2562914ad94fad4241de5e371097cc3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-10 21:32:40 +0600

    clousure

--- CodeForces/Contests_6/C_Monopati.cpp
+++ CodeForces/Contests_6/C_Monopati.cpp
@@ -22,7 +22,16 @@ int main()
             cin >> arr2[i];
         }
         bool flag = true;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > arr2[i])
+            {
+                flag = false;
+                break;
+            }
+        }
         
+
     }
     return 0;
 }
