22acf9fc18dd7998bae3a4eef7507953c9263ab8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-16 21:01:08 +0600

    added

--- CodeForces/Contests_8/B_Tab_Closing.cpp
+++ CodeForces/Contests_8/B_Tab_Closing.cpp
@@ -1,9 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long a, b, n;
+        cin >> a >> b >> n;
+        long long T = a / b;
+
+        if (b >= a || n <= T)
+        {
+            cout << 1 << endl;
+        }
+        else
+        {
+            cout << 2 << endl;
+        }
+    }
+
     return 0;
 }
