3600b2ee988794fb06f1b27147756e31f05991b0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-23 18:59:34 +0600

    added

--- HackerRank/Same_Component.cpp
+++ HackerRank/Same_Component.cpp
@@ -1,9 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+
+int N, M;
+vector<string> grid(1005);
+bool vis[1005][1005];
+vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
+bool valid(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    cin >> N >> M;
+    grid.resize(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> grid[i];
+    }
+    int Si, Sj, Di, Dj;
+    cin >> Si >> Sj;
+    cin >> Di >> Dj;
+
+    if (grid[Si][Sj] == '-'  || grid[Di][Dj] == '-')
+    {
+        cout << "NO" << endl;
+    }
+
+    memset(vis, false, sizeof(vis));
     
+    
+
     return 0;
 }
