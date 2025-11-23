271a982493ddb2e6f8bde7e5688b63a69acf7b64
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-23 16:17:17 +0600

    added

--- CodeForces/Contests_8/A_Dungeon_Equilibrium.cpp
+++ CodeForces/Contests_8/A_Dungeon_Equilibrium.cpp
@@ -11,11 +11,45 @@ int main()
     {
         int n;
         cin >> n;
-        int arr[n];
+        vector<int> arr(n);
         for (int i = 0; i < n; i++)
         {
             cin >> arr[i];
         }
+
+        vector<int> freq(n + 1, 0);
+        for (int x : arr)
+        {
+            if (x <= n)
+            {
+                freq[x]++;
+            }
+        }
+
+        long long ans = 0;
+        for (int v = 0; v <= n; v++)
+        {
+            int cnt = freq[v];
+            if (cnt == 0)
+            {
+                continue;
+            }
+
+            if (v == 0)
+            {
+                ans += cnt;
+            }
+            else if (cnt < v)
+            {
+                ans += cnt;
+            }
+            else
+            {
+                ans += min(cnt, cnt - v);
+            }
+        }
+
+        cout << ans << endl;
     }
 
     return 0;
