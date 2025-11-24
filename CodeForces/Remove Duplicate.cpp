0defd249de1a89e1ce4ffa6b5b6a34aa9bcab0f3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-24 08:57:50 +0600

    added

---
+++ CodeForces/Remove Duplicate.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int &val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+bool same(Node *head1, Node *head2)
+{
+    Node *tmp1 = head1;
+    Node *tmp2 = head2;
+    while (tmp1 != NULL && tmp2 != NULL)
+    {
+        if (tmp1->val != tmp2->val)
+        {
+            return false;
+        }
+        tmp1 = tmp1->next;
+        tmp2 = tmp2->next;
+    }
+    // 
+    if (tmp1 == NULL && tmp2 == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    Node *head1 = NULL;
+    Node *tail1 = NULL;
+    while (true)
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        else
+        {
+            insert_at_tail(head1, tail1, val);
+        }
+    }
+    //
+    Node *head2 = NULL;
+    Node *tail2 = NULL;
+    while (true)
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        else
+        {
+            insert_at_tail(head2, tail2, val);
+        }
+    }
+
+    if (same(head1, head2) == true)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
