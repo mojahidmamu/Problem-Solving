37cb2e28ecf52dd96583a15516f47bd71189efa1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-24 08:58:23 +0600

    added

--- CodeForces/Remove Duplicate.cpp
+++ CodeForces/Remove Duplicate.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Node
 {
 public:
@@ -28,37 +27,13 @@ void insert_at_tail(Node *&head, Node *&tail, int &val)
     }
 }
 
-bool same(Node *head1, Node *head2)
-{
-    Node *tmp1 = head1;
-    Node *tmp2 = head2;
-    while (tmp1 != NULL && tmp2 != NULL)
-    {
-        if (tmp1->val != tmp2->val)
-        {
-            return false;
-        }
-        tmp1 = tmp1->next;
-        tmp2 = tmp2->next;
-    }
-    // 
-    if (tmp1 == NULL && tmp2 == NULL)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    Node *head1 = NULL;
-    Node *tail1 = NULL;
+    Node *head = NULL;
+    Node *tail = NULL;
     while (true)
     {
         int val;
@@ -69,34 +44,10 @@ int main()
         }
         else
         {
-            insert_at_tail(head1, tail1, val);
-        }
-    }
-    //
-    Node *head2 = NULL;
-    Node *tail2 = NULL;
-    while (true)
-    {
-        int val;
-        cin >> val;
-        if (val == -1)
-        {
-            break;
-        }
-        else
-        {
-            insert_at_tail(head2, tail2, val);
+            insert_at_tail(head, tail, val);
         }
     }
 
-    if (same(head1, head2) == true)
-    {
-        cout << "YES" << endl;
-    }
-    else
-    {
-        cout << "NO" << endl;
-    }
-
+     
     return 0;
 }
