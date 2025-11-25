92ac4dc34218b382329237d0521076d318365b0f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-25 10:13:45 +0600

    added

---
+++ CodeForces/B_Down_with_Brackets.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        bool isBalanced;
+
+        if (isBalanced == true)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
