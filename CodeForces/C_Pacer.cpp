eef9a12e951fcd049dbea9260d6d99dbe9613d2e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-25 10:09:35 +0600

    added

--- CodeForces/C_Pacer.cpp
+++ CodeForces/C_Pacer.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
@@ -8,15 +9,65 @@ int main() {
     cin >> t;
     while (t--)
     {
-        int n, m;
+        int n;
+        long long m;
         cin >> n >> m;
-        vector<int> arr(n);
+        vector<long long> a(n);
+        vector<long long> b(n);
         for (int i = 0; i < n; i++)
         {
-            cin >> arr[i];
+            cin >> a[i] >> b[i];
         }
-        
+        long long prev_t = 0;
+        int prev_side = 0;
+        long long ans = 0;
+        bool impossible = false;
+        for (int i = 0; i < n; ++i)
+        {
+            long long t = a[i];
+            int side = b[i];
+            long long len = t - prev_t;
+            int needParity = prev_side ^ side; // 0 if same, 1 if different
+            if (len == 0)
+            {
+                if (needParity == 1)
+                { // cannot change side with zero minutes
+                    impossible = true;
+                    break;
+                }
+                // else k = 0
+            }
+            else
+            {
+                if ((len % 2) == needParity)
+                {
+                    ans += len;
+                }
+                else
+                {
+                    ans += (len - 1);
+                }
+            }
+            prev_t = t;
+            prev_side = side;
+        }
+        if (impossible)
+        {
+            cout << -1 << '\n';
+            continue;
+        }
+        // After last requirement, there may be remaining time up to m.
+        long long len = m - prev_t;
+        if (len < 0)
+        {
+            // should not happen if inputs valid (a_i <= m)
+            cout << -1 << '\n';
+            continue;
+        }
+        // final segment has no required end-side, so we can simply run every minute:
+        ans += len;
+        cout << ans << '\n';
     }
-    
+
     return 0;
 }
