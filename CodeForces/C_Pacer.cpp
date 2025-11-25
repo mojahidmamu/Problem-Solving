5d42452470c245d28cb6091eb1a4a3ccda232264
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-25 09:12:02 +0600

    added

--- CodeForces/C_Pacer.cpp
+++ CodeForces/C_Pacer.cpp
@@ -4,6 +4,19 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        
+    }
     
     return 0;
 }
