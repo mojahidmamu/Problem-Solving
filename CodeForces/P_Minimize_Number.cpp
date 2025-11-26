202c9b30585178b80184de761531c430297e083a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-26 17:11:02 +0600

    added

--- CodeForces/P_Minimize_Number.cpp
+++ CodeForces/P_Minimize_Number.cpp
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int n;
+    cin >> n;
+    long long arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
     return 0;
 }
