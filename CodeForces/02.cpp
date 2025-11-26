0745dfb2363b15963bd50730672fa04982a134c5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-26 23:28:12 +0600

    added

---
+++ CodeForces/02.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insret_at_tail(Node *head, Node *tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tail->next = newNode;
+        newNode = tail;
+    }
+}
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Node *head = NULL;
+        Node *tail = NULL;
+        while (true)
+        {
+            int val;
+            cin >> val;
+            if (val == -1)
+            {
+                break;
+            }
+            else
+            {
+                insret_at_tail(head, tail, val);
+            }
+        }
+
+        int x;
+        cin >> x;
+
+        int index = 0;
+        Node *tmp = head;
+        while (tmp != NULL)
+        {
+            if (tmp->val == x)
+            {
+                cout << index << endl;
+                break;
+            }
+            else
+            {
+                tmp = tmp->next;
+                index++;
+            }
+        }
+    }
+
+    return 0;
+}
