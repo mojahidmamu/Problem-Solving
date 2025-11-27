3013a2c229aeed798925edfc74efeb0576ea8b6d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-27 12:29:42 +0600

    added

--- CodeForces/Y_Range_sum_query.cpp
+++ CodeForces/Y_Range_sum_query.cpp
@@ -4,6 +4,20 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    int n, q;
+    cin >> n >> q;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    while (q--)
+    {
+        int L, R;
+        cin >> L >> R;
+    }
+    
     
     return 0;
 }
