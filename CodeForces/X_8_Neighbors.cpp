0bfdcbe69fa5ef116fd6dfe31d1b45d556c18652
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-27 12:23:37 +0600

    added

--- CodeForces/X_8_Neighbors.cpp
+++ CodeForces/X_8_Neighbors.cpp
@@ -18,7 +18,32 @@ int main()
 
     int X, Y;
     cin >> X >> Y;
-    
 
+    X--;
+    Y--;
+
+    int dr[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int dc[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+    bool ok = true;
+    for (int k = 0; k < 8; k++)
+    {
+        int nr = X + dr[k];
+        int nc = Y + dc[k];
+
+        if (nr >= 0 && nr < n && nc >= 0 && nc < m)
+        {
+            if (arr[nr][nc] != "x")
+            {
+                ok = false;
+                break;
+            }
+        }
+    }
+
+    if (ok)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
     return 0;
 }
