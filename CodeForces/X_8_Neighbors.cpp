fd1df4a2dcd855609a5a5c73a0e9ed0146966b35
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-27 12:07:54 +0600

    added

---
+++ CodeForces/X_8_Neighbors.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int n, m;
+    cin >> n >> m;
+    string arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    return 0;
+}
