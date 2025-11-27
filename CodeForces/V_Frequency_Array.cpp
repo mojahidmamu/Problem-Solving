cede6f664c3056fbf0229b387217eea82b776ef7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-27 12:04:34 +0600

    added

--- CodeForces/V_Frequency_Array.cpp
+++ CodeForces/V_Frequency_Array.cpp
@@ -7,10 +7,21 @@ int main()
     // --- your code here ---
     int n, m;
     cin >> n >> m;
-    int arr[n];
+    vector<int> freq(m + 1, 0);
+
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        int x;
+        cin >> x;
+        if (1 <= x && x <= m)
+        {
+            freq[x]++;
+        }
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        cout << freq[i] << endl;
     }
 
     return 0;
