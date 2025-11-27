d40671a2d161ebeb51313c379a5ae542c39a5986
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-27 11:49:25 +0600

    added

--- CodeForces/U_Is_B_a_subsequence_of_A.cpp
+++ CodeForces/U_Is_B_a_subsequence_of_A.cpp
@@ -1,9 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int n, m;
+    cin >> n >> m;
+    long long arr1[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr1[i];
+    }
+    //  
+    long long arr2[m];
+    for (int i = 0; i < m; i++)
+    {
+        cin >> arr2[i];
+    }
+
     return 0;
 }
