328b08558eeffbc8b0abe32bd743a1f0ab934180
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-27 12:39:11 +0600

    added

--- CodeForces/Y_Range_sum_query.cpp
+++ CodeForces/Y_Range_sum_query.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     int n, q;
     cin >> n >> q;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+    vector<long long> arr(n + 1), prefix(n + 1, 0);
+    for (int i = 1; i <= n; i++)
     {
         cin >> arr[i];
+        prefix[i] = prefix[i -1] + arr[i];
     }
 
     while (q--)
     {
         int L, R;
         cin >> L >> R;
+
+        long long sum = prefix[R] - prefix[L - 1];
+
+        cout << sum << endl;
     }
-    
-    
+
     return 0;
 }
