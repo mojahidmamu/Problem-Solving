d2f58c8e987fa96c93d03ffe227f9477aa4daa26
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-28 18:38:19 +0600

    added

---
+++ CodeForces/E_Hady_Rides_the_Train.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    long long id;
+    cin >> id;
+    
+    return 0;
+}
