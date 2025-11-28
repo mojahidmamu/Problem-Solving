c53fed2d04a2cc1926bc4c741c70281f23e9f376
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-28 12:30:09 +0600

    added

--- CodeForces/N_Check_Code.cpp
+++ CodeForces/N_Check_Code.cpp
@@ -1,9 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int A, B;
+    cin >> A >> B;
+    string s;
+    cin >> s;
+    int pos = A + 1;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (pos == '-')
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
     return 0;
 }
