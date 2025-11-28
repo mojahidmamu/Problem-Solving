8e62107b6c14f77c3d4a5a1f18233f55079a01bc
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-28 18:34:56 +0600

    added

--- CodeForces/C_Finding_Minimums.cpp
+++ CodeForces/C_Finding_Minimums.cpp
@@ -1,9 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int N, K;
+    cin >> N >> K;
+    long long arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < N; i += K)
+    {
+        int end = min(i + K, N);
+        long long mn = LLONG_MAX;
+        for (int j = i; j < end; j++)
+        {
+            mn = min(mn, arr[j]);
+        }
+        cout << mn << " ";
+    }
+    cout << endl;
+
     return 0;
 }
