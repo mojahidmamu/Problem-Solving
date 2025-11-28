60e7944ec1ff5b047753a0cbf6683a24a144194e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-28 19:21:07 +0600

    added

--- CodeForces/G_Construct_the_Sum.cpp
+++ CodeForces/G_Construct_the_Sum.cpp
@@ -1,16 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    // --- your code here ---
+
     int t;
     cin >> t;
-    while (t--)
-    {
-        int n, s;
+    while (t--) {
+        long long n, s;
         cin >> n >> s;
+
+        long long maxSum = 1LL * n * (n + 1) / 2;
+        if (s > maxSum) {
+            cout << -1 << '\n';
+            continue;
+        }
+
+        vector<long long> ans;
+        for (long long i = 1; i <= n && s > 0; ++i) {
+            if (s >= i) {
+                ans.push_back(i);
+                s -= i;
+            }
+        }
+
+        if (s != 0) {
+            cout << -1 << '\n';
+        } else { 
+            reverse(ans.begin(), ans.end());
+            for (size_t i = 0; i < ans.size(); ++i) {
+                if (i) cout << ' ';
+                cout << ans[i];
+            }
+            cout << '\n';
+        }
     }
 
     return 0;
