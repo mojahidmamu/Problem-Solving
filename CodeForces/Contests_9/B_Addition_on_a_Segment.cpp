0f7286125b46cef3316d4f3138ed577cf24c0255
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-28 21:14:49 +0600

    added

--- CodeForces/Contests_9/B_Addition_on_a_Segment.cpp
+++ CodeForces/Contests_9/B_Addition_on_a_Segment.cpp
@@ -11,10 +11,27 @@ int main()
     {
         int n;
         cin >> n;
-        int arr[n];
+        vector<int> arr(n);
+        int zero_cnt = 0;
+        set<int> s;
+
         for (int i = 0; i < n; i++)
         {
             cin >> arr[i];
+            if (arr[i] == 0)
+            {
+                zero_cnt++;
+            }
+            s.insert(arr[i]);
+        }
+
+        if (s.size() == 1 && *s.begin() > 0)
+        {
+            cout << 1 << endl;
+        }
+        else
+        {
+            cout << n - zero_cnt << endl;
         }
     }
 
