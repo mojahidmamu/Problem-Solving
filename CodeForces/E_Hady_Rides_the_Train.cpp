1d220edfa02bddcde71ffd7b671a852b062681f4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-28 18:51:56 +0600

    added

--- CodeForces/E_Hady_Rides_the_Train.cpp
+++ CodeForces/E_Hady_Rides_the_Train.cpp
@@ -1,11 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     long long id;
     cin >> id;
-    
+
+    long long row = id / 4;
+    long long pos = id % 4;
+    int col;
+    if (row % 2 == 0)
+    {
+        col = pos;
+    }
+    else
+    {
+        col = 3 - pos;
+    }
+    cout << row << " " << col << endl;
     return 0;
 }
