70c6c230e463c651a811a4e8617957782ecf2f33
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-29 19:12:00 +0600

    added

---
+++ CodeForces/J_Primes_from_1_to_n.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int n;
+    cin >> n;
+    for (int i = 2; i <= n; i++) // 2 to n
+    {
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+        {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
