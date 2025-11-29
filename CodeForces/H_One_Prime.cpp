7ee11e1358490153fc7debd26d42a69074379145
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-29 18:52:09 +0600

    added

---
+++ CodeForces/H_One_Prime.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int n;
+    cin >> n;
+    bool isPrime = false;
+
+    if (isPrime == true)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
