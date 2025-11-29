998d65dcb9bd16eeda9cabaf4267743fe27a40ca
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-29 19:24:17 +0600

    added

---
+++ CodeForces/L_GCD.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int A, B;
+    cin >> A >> B;
+
+    cout << __gcd(A, B) << endl;
+
+        return 0;
+}
