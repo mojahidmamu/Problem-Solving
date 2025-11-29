931578a3c716fd4f95189ab6d11ad13803887211
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-29 18:50:36 +0600

    added

--- CodeForces/I_Palindrome.cpp
+++ CodeForces/I_Palindrome.cpp
@@ -1,12 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     string s;
     cin >> s;
+    string original = s;
     reverse(s.begin(), s.end());
-    
+
+    while (s.size() > 1 && s[0] == '0')
+    {
+        s.erase(s.begin());
+    }
+
+    cout << s << endl;
+
+    if (s == original)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
     return 0;
 }
