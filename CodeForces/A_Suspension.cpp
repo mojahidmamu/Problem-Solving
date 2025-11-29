41bfc9075c91f4177dc276b5a41cc0dfbe12c69a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-29 20:41:12 +0600

    added

--- CodeForces/A_Suspension.cpp
+++ CodeForces/A_Suspension.cpp
@@ -1,17 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     int t;
     cin >> t;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++)
+    while (t--)
     {
-        cin >> arr[i];
+        int n, y, r;
+        cin >> n >> y >> r;
+
+        int suspended_from_yellow = y / 2;
+        int total_suspended = r + suspended_from_yellow;
+
+        total_suspended = min(total_suspended, n);
+        cout << total_suspended << endl;
     }
-    
-    
+
     return 0;
 }
