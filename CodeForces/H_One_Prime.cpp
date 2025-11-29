cfa151b2698123a770f8accb29463229b6e7e7a7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-29 18:56:22 +0600

    added

--- CodeForces/H_One_Prime.cpp
+++ CodeForces/H_One_Prime.cpp
@@ -7,7 +7,23 @@ int main()
     // --- your code here ---
     int n;
     cin >> n;
-    bool isPrime = false;
+    bool isPrime = true;
+
+    if (n <= 1)
+    {
+        isPrime = false;
+    }
+    else
+    {
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+    }
 
     if (isPrime == true)
     {
