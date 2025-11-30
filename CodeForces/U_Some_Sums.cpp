2c13c9df726db21b5c5069c8dbba4e8697bdbe2b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-30 09:55:01 +0600

    added

--- CodeForces/U_Some_Sums.cpp
+++ CodeForces/U_Some_Sums.cpp
@@ -1,5 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int digit_sum(int x)
+{
+    int s = 0;
+    while (x > 0)
+    {
+        s += x % 10;
+        x /= 10;
+    }
+    return s;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -7,5 +19,17 @@ int main()
     // --- your code here ---
     int N, A, B;
     cin >> N >> A >> B;
+
+    int sum = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        if (A <= digit_sum(i) && digit_sum(i) <= B)
+        {
+            sum += i;
+        }
+    }
+
+    cout << sum << endl;
+
     return 0;
 }
