3211f661645d1feb71139b37424002eafdd9cebe
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-30 10:01:44 +0600

    added

---
+++ CodeForces/W_Shape_3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int n;
+    cin >> n;
+    return 0;
+}
