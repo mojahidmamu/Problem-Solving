6bbcb8d51072bfe79dc9ab447f6253ef2201e9cf
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-30 11:10:41 +0600

    added

--- CodeForces/Y_Easy_Fibonacci.cpp
+++ CodeForces/Y_Easy_Fibonacci.cpp
@@ -7,9 +7,25 @@ int main()
     // --- your code here ---
     int n;
     cin >> n;
-    for (int i = 0; i < n; i++)
+
+    int a = 0, b = 1;
+    for (int i = 1; i <= n; i++)
     {
-       
+        if (i == 1)
+        {
+            cout << a << " ";
+        }
+        else if (i == 2)
+        {
+            cout << b << " ";
+        }
+        else
+        {
+            int c = a + b;
+            cout << c << " ";
+            a = b;
+            b = c;
+        }
     }
 
     return 0;
