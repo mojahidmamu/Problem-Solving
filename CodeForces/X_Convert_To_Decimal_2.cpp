664ff613cac9207f7b0c2c833ab5409d0026375e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-30 11:47:42 +0600

    added

--- CodeForces/X_Convert_To_Decimal_2.cpp
+++ CodeForces/X_Convert_To_Decimal_2.cpp
@@ -11,6 +11,21 @@ int main()
     {
         int n;
         cin >> n;
+
+        int ones = 0;
+        int temp = n;
+
+        while (temp)
+        {
+            if (temp & 1)
+            {
+                ones++;
+            }
+            temp >>= 1;
+        }
+        unsigned int result = (1U << ones) - 1;
+
+        cout << result << "\n";
     }
 
     return 0;
