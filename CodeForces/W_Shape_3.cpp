4e0a307b173633af2711e8b740f64972da3826f5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-30 10:30:03 +0600

    added

--- CodeForces/W_Shape_3.cpp
+++ CodeForces/W_Shape_3.cpp
@@ -1,10 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     int n;
     cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int space = 1; space <= n - i; space++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    //
+    for (int j = 1; j <= 2 * n - 1; j++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+
+    //
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int space = 1; space <= n - i; space++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
