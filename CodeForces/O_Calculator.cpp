0cba63e2690daa3e8600f7bd0950b96347bcabd8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-01 10:07:33 +0600

    added

---
+++ CodeForces/O_Calculator.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int A;
+    char s;
+    int B;
+    cin >> A >> s >> B;
+    int result;
+    if (s == '+')
+    {
+        result = A + B;
+    }
+    else if (s == '-')
+    {
+        result = A - B;
+    }
+    else if (s == '*')
+    {
+        result = A * B;
+    }
+    else if (s == '/')
+    {
+        result = A / B;
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
