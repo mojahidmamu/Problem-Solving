9a74eeb533a61b47c8f66ba986acd2cf7dfbbae2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-01 09:57:42 +0600

    added

---
+++ CodeForces/P_First_digit.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int n;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << "EVEN" << endl;
+    }
+    else
+    {
+        cout << "ODD" << endl;
+    }
+
+    return 0;
+}
