67847377f6b24d48d8c5da2108b6f0a6e3909be3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-01 09:26:32 +0600

    added

---
+++ CodeForces/C_Simple_Calculator.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int X, Y;
+    cin >> X >> Y;
+    cout << X << " + " << Y << " = " << X + Y << endl;
+    cout << X << " * " << Y << " = " << X * Y << endl;
+    cout << X << " - " << Y << " = " << X - Y << endl;
+    return 0;
+}
