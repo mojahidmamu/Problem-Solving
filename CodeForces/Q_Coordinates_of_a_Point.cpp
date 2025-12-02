eecc14e20846e7e4c9c759d3fd89293685cb07b3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-02 14:29:28 +0600

    added

--- CodeForces/Q_Coordinates_of_a_Point.cpp
+++ CodeForces/Q_Coordinates_of_a_Point.cpp
@@ -1,11 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    int X, Y;
+    float X, Y;
     cin >> X >> Y;
+    if (X == 0 && Y == 0)
+    {
+        cout << "Origem" << endl;
+    }
+    else if (X = 0)
+    {
+        cout << "Eixo X" << endl;
+    }
+    else if (Y == 0)
+    {
+        cout << "Eixo Y" << endl;
+    }
+    else if (condition)
+    {
+        /* code */
+    }
     
+
     return 0;
 }
