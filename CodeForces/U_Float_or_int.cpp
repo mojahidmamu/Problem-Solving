640984e0f39e81dbc473e5e99ba0676da9888dcf
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-02 13:29:52 +0600

    added

--- CodeForces/U_Float_or_int.cpp
+++ CodeForces/U_Float_or_int.cpp
@@ -1,11 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    int n;
+    double n;
     cin >> n;
-    
+
+    long long intPart = (long long)n;
+    double decimalPart = n - intPart;
+    if (decimalPart == 0)
+    {
+        cout << "int " << intPart << endl;
+    }
+    else
+    {
+        cout << "float " << intPart << " " << fixed << setprecision(3) << decimalPart << endl;
+    }
+
     return 0;
 }
