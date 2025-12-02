1cf4e99a62e1446535c235ba3eba121eb6c41099
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-02 13:02:33 +0600

    added

--- CodeForces/Y_The_last_2_digits.cpp
+++ CodeForces/Y_The_last_2_digits.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    long long a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    long long result = 1;
+    result = ((a % 100) * (b % 100) * (c % 100) * (d % 100)) % 100;
+
+    cout << setw(2) << setfill('0') << result << endl;
     return 0;
 }
