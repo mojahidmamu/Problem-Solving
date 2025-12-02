dc1ac9e8a6d06db3c92e1aa33e078c978c45c7d4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-02 14:37:51 +0600

    added

--- CodeForces/S_Interval.cpp
+++ CodeForces/S_Interval.cpp
@@ -1,11 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    int x;
-    cin >> x ;
-    
+    double x;
+    cin >> x;
+    if (0 <= x && x <= 25)
+    {
+        cout << "Interval [0,25]" << endl;
+    }
+    else if (25 < x && x <= 50)
+    {
+        cout << "Interval (25,50]" << endl;
+    }
+    else if (50 < x && x <= 75)
+    {
+        cout << "Interval [50,75]" << endl;
+    }
+    else if (75 < x && x <= 100)
+    {
+        cout << "Interval [75,100]" << endl;
+    }
+    else
+    {
+        cout << "Out of Intervals" << endl;
+    }
+
     return 0;
 }
