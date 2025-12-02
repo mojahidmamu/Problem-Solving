db27b5ac3161e91ed3a5457a996d701f75997ed3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-02 12:48:58 +0600

    added

--- CodeForces/X_Two_intervals.cpp
+++ CodeForces/X_Two_intervals.cpp
@@ -5,8 +5,18 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    int l1, r1, l2, r2;
+    long long l1, r1, l2, r2;
     cin >> l1 >> r1 >> l2 >> r2;
+    long long left = max(l1, l2);
+    long long right = min(r1, r2);
+    if (left <= right)
+    {
+        cout << l2 << " " << r1 << endl;
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
 
     return 0;
 }
