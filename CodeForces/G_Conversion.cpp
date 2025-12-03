2afd22c6ad2fcd5454bcc9331e0b2e7d7791ff62
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-03 11:49:07 +0600

    added

---
+++ CodeForces/G_Conversion.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    string s;
+    cin >> s;
+    for (char &c : s)
+    {
+        if (isupper(c))
+        {
+            c = tolower(c);
+        }
+        else if (islower(c))
+        {
+            c = toupper(c);
+        }
+
+        if (c == ',')
+        {
+            c = ' ';
+        }
+    }
+
+    cout << s << endl;
+
+    return 0;
+}
