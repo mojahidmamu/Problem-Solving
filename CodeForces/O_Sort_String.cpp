cdc42198ad193ac2e405daa764c6d27f1db3b44b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-03 12:20:55 +0600

    added

---
+++ CodeForces/O_Sort_String.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    cout << s << endl;
+
+    return 0;
+}
