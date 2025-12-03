7c1bec51e9547ca77a444e8959c5ba0c1e7e7a34
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-03 11:50:20 +0600

    added

---
+++ CodeForces/H_Good_or_Bad.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    string s;
+    cin >> s;
+    
+    return 0;
+}
