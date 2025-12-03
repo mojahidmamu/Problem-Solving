ca650e647c2959dbe69bde58efd98cab7e8729b0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-03 11:40:53 +0600

    added

---
+++ CodeForces/A_Create_A_New_String.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    string s, t;
+    cin >> s >> t;
+    cout << s.size() << " " << t.size() << endl;
+    cout << s << " " << t << endl;
+    return 0;
+}
