408fd3d1f33f687b261c3e24e20fe8711a189ef4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-04 15:30:30 +0600

    added

--- CodeForces/U_New_Words.cpp
+++ CodeForces/U_New_Words.cpp
@@ -1,9 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    string s;
+    cin >> s;
+    // Print number of times that "EGYPT" word can be formed from S's characters.
+    string target = "EGYPT";
+    map<char, int> char_count;
+    for (char c : s)
+    {
+        c = toupper(c);
+        char_count[c]++;
+    }
+    int min_count = INT_MAX;
+    for (char c : target)
+    {
+        if (char_count.find(c) == char_count.end())
+        {
+            min_count = 0;
+            break;
+        }
+        min_count = min(min_count, char_count[c]);
+    }
+    cout << min_count << "\n";
+    // --- your code here ---
     return 0;
 }
