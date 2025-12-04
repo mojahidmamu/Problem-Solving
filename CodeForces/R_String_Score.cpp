105c4be4e1559547b7b9b9217e7429d7e24a6118
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-04 11:37:00 +0600

    added

--- CodeForces/R_String_Score.cpp
+++ CodeForces/R_String_Score.cpp
@@ -1,9 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int score = 0;
+    for (char c : s)
+    {
+        if (c == 'V')
+        {
+            score += 5;
+        }
+        else if (c == 'W')
+        {
+            score += 2;
+        }
+        else if (c == 'X')
+        {
+        }
+        else if (c == 'Y')
+        {
+        }
+        else if (c == 'Z')
+        {
+            score += 1;
+        }
+    }
+
+    cout << score << "\n";
     return 0;
 }
