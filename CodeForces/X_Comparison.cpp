86a66cd4a787e523f22151fedfa0f87ae69a5624
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-04 15:50:56 +0600

    added

--- CodeForces/X_Comparison.cpp
+++ CodeForces/X_Comparison.cpp
@@ -6,9 +6,28 @@ int main()
     cin.tie(nullptr);
     // --- your code here ---
     string s;
-    cin >> s; 
-    // Print the smallest string that can be obtained.
-    sort(s.begin(), s.end());
-    cout << s << "\n";
+    cin >> s;
+    // Print the smallest string that can be obtained
+    int n = s.length();
+    if (n == 1)
+    {
+        cout << s << "\n";
+    }
+
+    string answer = s;
+    for (int i = 1; i < n; i++)
+    {
+        string X = s.substr(0, i);
+        string Y = s.substr(i);
+
+        sort(X.begin(), X.end());
+        sort(Y.begin(), Y.end());
+
+        string candidate = X + Y;
+        answer = min(answer, candidate);
+    }
+    cout << answer << "\n";
+    // --- your code here ---
+
     return 0;
 }
