d3b5f5c1154585b505ad741c80e87db7436021f3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-07 10:35:37 +0600

    added

--- CodeForces/O_Five_in_One.cpp
+++ CodeForces/O_Five_in_One.cpp
@@ -1,16 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int primeCheck(int num)
+int countPrimeNumber(int arr[], int n)
 {
-    if (num <= 1)
-        return 0;
-    for (int i = 2; i * i <= num; i++)
+    int count = 0;
+    for (int i = 0; i < n; i++)
     {
-        if (num % i == 0)
-            return 0;
+        bool isPrime = true;
+        if (arr[i] <= 1)
+            isPrime = false;
+        for (int j = 2; j <= sqrt(arr[i]); j++)
+        {
+            if (arr[i] % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime)
+            count++;
     }
-    return 1;
+    return count;
+}
+
+int countPalirome(int arr[], int n)
+{
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int num = arr[i];
+        int rev = 0;
+        while (num > 0)
+        {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+        if (rev == arr[i])
+            count++;
+    }
+    return count;
+}
+
+int countDivisors(int arr[], int n)
+{
+    int totalDivisors = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int num = arr[i];
+        int divisors = 0;
+        for (int j = 1; j <= sqrt(num); j++)
+        {
+            if (num % j == 0)
+            {
+                divisors++;
+                if (j != num / j)
+                    divisors++;
+            }
+        }
+        totalDivisors += divisors;
+    }
+    return totalDivisors;
 }
 
 int main()
@@ -27,6 +76,15 @@ int main()
     }
     int maximum = *max_element(arr, arr + n);
     int minimum = *min_element(arr, arr + n);
-    
+    int primeCount = countPrimeNumber(arr, n);
+    int palindromeCount = countPalirome(arr, n);
+    int totalDivisors = countDivisors(arr, n);
+
+    // print:
+    cout << "Maximum: " << maximum << "\n";
+    cout << "Minimum: " << minimum << "\n";
+    cout << "Prime Count: " << primeCount << "\n";
+    cout << "Palindrome Count: " << palindromeCount << "\n";
+    cout << "Total Divisors: " << totalDivisors << "\n";
     return 0;
 }
