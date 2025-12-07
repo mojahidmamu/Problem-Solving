45fcb7db8da7d541f6d072ed481b83e1b659aa6c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-07 09:44:42 +0600

    added

--- CodeForces/C_Wonderful_Number.cpp
+++ CodeForces/C_Wonderful_Number.cpp
@@ -1,22 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool wonderful_number(long long n)
+bool isOdd(long long n)
+{
+    return n % 2 != 0;
+}
+
+bool isBinaryPalindrome(long long n)
 {
     // A number is wonderful if this number is odd and its binary representation is palindrome.
-    if (n % 2 == 0)
-    {
-        return false;
-    }
-    string binary_representation = "";
+    string binary = "";
     while (n > 0)
     {
-        binary_representation += (n % 2 == 0) ? '0' : '1';
+        binary += (n % 2 == 0) ? '0' : '1';
         n /= 2;
     }
-    string reversed_representation = binary_representation;
-    reverse(reversed_representation.begin(), reversed_representation.end());
-    return binary_representation == reversed_representation;
+    string revBinary = binary;
+    reverse(revBinary.begin(), revBinary.end());
+    return binary == revBinary;
 }
 
 int main()
@@ -26,7 +27,7 @@ int main()
     // --- your code here ---
     long long n;
     cin >> n;
-    if (wonderful_number(n))
+    if (isOdd(n) && isBinaryPalindrome(n))
     {
         cout << "YES\n";
     }
