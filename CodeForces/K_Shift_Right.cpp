7f50b7f583d3ec81dd5ca39f065b11365fff490d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-07 10:16:50 +0600

    added

--- CodeForces/K_Shift_Right.cpp
+++ CodeForces/K_Shift_Right.cpp
@@ -1,15 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+
+int shiftRight(int arr[], int n, int x)
+{
+    x = x % n; 
+    reverse(arr, arr + n);
+    reverse(arr, arr + x);
+    reverse(arr + x, arr + n);
+    return 0;
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     int n, x;
     cin >> n >> x;
     int arr[n];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
-    
+
+    shiftRight(arr, n, x); 
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
     return 0;
 }
