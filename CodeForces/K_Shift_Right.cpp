789d00534152b8304a05741f0a79e894db455416
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-07 10:11:01 +0600

    added

--- CodeForces/K_Shift_Right.cpp
+++ CodeForces/K_Shift_Right.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    int n, x;
+    cin >> n >> x;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
     
     return 0;
 }
