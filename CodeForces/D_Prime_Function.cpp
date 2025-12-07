21f3f18bcbfe0da1cd26bc1dd12b123357c8af40
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-07 09:47:26 +0600

    added

---
+++ CodeForces/D_Prime_Function.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(long long n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (long long i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        isPrime(n) ? cout << "YES\n" : cout << "NO\n";
+    }
+    return 0;
+}
