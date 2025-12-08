f9798f19ff2f83f0ca00dba53c6e84c30c9b5cca
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-08 21:46:27 +0600

    added

--- CodeForces/H_GCD.cpp
+++ CodeForces/H_GCD.cpp
@@ -1,19 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     long long a, b;
     cin >> a >> b;
-    // Compute GCD of a and b
-    while (b != 0) {
-        long long temp = b;
-        b = a % b;
-        a = temp;
-    }
-    cout << a << "\n";
-
+    int GCD = __gcd(a, b);
+    int LCM = (a * b) / GCD;
+    cout << GCD << " " << LCM << "\n";
 
     return 0;
 }
