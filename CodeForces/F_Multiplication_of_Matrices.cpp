d5ba88e94477cb514f71e51bfbea5404d4af3e18
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-08 20:45:51 +0600

    added

---
+++ CodeForces/F_Multiplication_of_Matrices.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int rA, cA;
+    cin >> rA >> cA;
+    int A[rA][cA];
+    for (int i = 0; i < rA; i++)
+    {
+        for (int j = 0; j < cA; j++)
+        {
+            cin >> A[i][j];
+        }
+    }
+
+    //
+    int rB, cB;
+    cin >> rB >> cB;
+    int B[rB][cB];
+    for (int i = 0; i < rB; i++)
+    {
+        for (int j = 0; j < cB; j++)
+        {
+            cin >> B[i][j];
+        }
+    }
+
+    return 0;
+}
