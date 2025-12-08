b3f10cdb1923335c14fa3079cbe2cf9b02295e56
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-08 20:28:37 +0600

    added

--- CodeForces/A_Power_Of_Two.cpp
+++ CodeForces/A_Power_Of_Two.cpp
@@ -5,14 +5,13 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    int n;
+    long long n;
     cin >> n;
-    int power = 1;
-    while (power < n)
-    {
-        power *= 2;
-    }
-    cout << power << "\n";
+    //   Determine whether N is power of 2 or not.
+    if (n > 0 && (n & (n - 1)) == 0)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
     // ---- end of your code ----
     return 0;
 }
