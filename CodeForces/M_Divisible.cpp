bf29bb96f09044c6e548654db84d61fa7a7949bb
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-09 08:55:41 +0600

    added

--- CodeForces/M_Divisible.cpp
+++ CodeForces/M_Divisible.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    long long int n, x;
+    cin >> n >> x;
+    if (n % x == 0)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+
     return 0;
 }
