c787f475813ef22a015ea28bbbf1e8d6c7bbea9a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-11 14:11:47 +0600

    added

--- CodeForces/E_Interval_Sweep.cpp
+++ CodeForces/E_Interval_Sweep.cpp
@@ -6,6 +6,19 @@ int main() {
     // --- your code here ---
     int a, b;
     cin >> a >> b;
+    int total  = a + b;
+    if (total == 0)
+    {
+        cout << "NO" << "\n";
+    }
+    else if (abs(a - b) <=  1)
+    {
+        cout << "YES" << "\n";
+    }
+    else
+    {
+        cout << "NO" << "\n";
+    }
     
     return 0;
 }
