8f62d4df6bcd77c3eff91fa926ac47e4ec1fd135
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-11 14:36:55 +0600

    added

--- CodeForces/I_Lucky_Numbers.cpp
+++ CodeForces/I_Lucky_Numbers.cpp
@@ -7,21 +7,10 @@ int main()
     // --- your code here ---
     int n;
     cin >> n;
-    //   A number of two digits is lucky if one of its digits is divisible by the other.
-    bool is_lucky = true;
 
     int firstDigit = n / 10;
     int lastDigit = n % 10;
     if (firstDigit % lastDigit == 0 || lastDigit % firstDigit == 0)
-    {
-        is_lucky = true;
-    }
-    else
-    {
-        is_lucky = false;
-    }
-
-    if (is_lucky == true)
     {
         cout << "YES" << "\n";
     }
