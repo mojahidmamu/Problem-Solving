3d4f5504e0787521eae0ca8986ebb15ca033e80f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-11 10:29:46 +0600

    added

--- CodeForces/A_String.cpp
+++ CodeForces/A_String.cpp
@@ -1,16 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+
+int solve(const string &s)
+{
+    int count = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '1' && (i == 0 || s[i - 1] == '0'))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         string s;
         cin >> s;
-        int n = s.size();
-        
+        int result = solve(s); // Assume solve is a function defined elsewhere
+        cout << result << "\n";
     }
     return 0;
 }
