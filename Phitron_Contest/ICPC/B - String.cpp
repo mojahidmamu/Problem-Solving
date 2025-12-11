7a50aa64863649067361c36e57188d9bb65aa5c6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-11 09:35:30 +0600

    added

---
+++ Phitron_Contest/ICPC/B - String.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int n = s.length();
+        if (n > 10)
+        {
+            cout << s[0] << n - 2 << s[n - 1] << "\n";
+        }
+        else
+        {
+            cout << s << "\n";
+        }
+    }
+    return 0;
+}
