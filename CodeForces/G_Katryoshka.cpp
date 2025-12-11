bb3945d6fdb3169bf5f314c0711816fd13b0d63c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-11 15:07:26 +0600

    added

--- CodeForces/G_Katryoshka.cpp
+++ CodeForces/G_Katryoshka.cpp
@@ -7,18 +7,15 @@ int main()
     // --- your code here ---
     long long n, m, k;
     cin >> n >> m >> k;
-    // First match as many eye-mouth pairs (1 eye + 1 mouth)
+
     long long type3 = min(n, m);
 
-    // Remaining eyes
     long long remain_eye = n - type3;
 
-    // Each 2 remaining eyes makes a type1 doll
     long long type1 = remain_eye / 2;
 
     long long total = type3 + type1;
 
-    // But limited by bodies
     cout << min(total, k) << "\n";
 
     return 0;
