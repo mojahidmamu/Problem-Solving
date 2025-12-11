8d51d887e5e22f066f1e4fa06577405c0911b30b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-11 11:34:20 +0600

    added

--- CodeForces/B_Mex_Master.cpp
+++ CodeForces/B_Mex_Master.cpp
@@ -12,12 +12,32 @@ int main()
         int n;
         cin >> n;
         vector<int> a(n);
+        int cont0 = 0, cont1 = 0;
         for (int i = 0; i < n; i++)
         {
             cin >> a[i];
+            if (a[i] == 0)
+            {
+                cont0++;
+            }
+            else
+            {
+                cont1++;
+            }
         }
 
-        
+        if (cont0 == 0)
+        {
+            cout << 0 << "\n";
+        }
+        else if (cont0 == 1 || cont1 == 0)
+        {
+            cout << 1 << "\n";
+        }
+        else
+        {
+            cout << 2 << "\n";
+        }
     }
     return 0;
 }
