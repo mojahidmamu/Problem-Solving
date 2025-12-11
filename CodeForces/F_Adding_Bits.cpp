824ebe6fa53f2a630561a0d6d6550862b173b32d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-11 14:14:42 +0600

    added

---
+++ CodeForces/F_Adding_Bits.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    unsigned int a, b;
+    cin >> a >> b;
+    cout << (a ^ b) << "\n";
+
+    return 0;
+}
