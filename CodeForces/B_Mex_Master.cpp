7facb1c03cf4226899289dfc5162e490dd977df0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-11 11:47:35 +0600

    added

--- CodeForces/B_Mex_Master.cpp
+++ CodeForces/B_Mex_Master.cpp
@@ -1,43 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    // --- your code here ---
+
     int t;
     cin >> t;
     while (t--)
     {
         int n;
         cin >> n;
-        vector<int> a(n);
-        int cont0 = 0, cont1 = 0;
+
+        int cnt0 = 0, cnt1 = 0;
         for (int i = 0; i < n; i++)
         {
-            cin >> a[i];
-            if (a[i] == 0)
-            {
-                cont0++;
-            }
-            else
-            {
-                cont1++;
-            }
+            int x;
+            cin >> x;
+            if (x == 0)
+                cnt0++;
+            else if (x == 1)
+                cnt1++;
         }
 
-        if (cont0 == 0)
-        {
-            cout << 0 << "\n";
-        }
-        else if (cont0 == 1 || cont1 == 0)
-        {
-            cout << 1 << "\n";
-        }
-        else
-        {
-            cout << 2 << "\n";
-        }
+        int ans = min(cnt0, 2) - 1 + min(cnt1, 1);
+        cout << ans << "\n";
     }
+
     return 0;
 }
