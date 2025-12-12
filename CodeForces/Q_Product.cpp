9df63e7c2c90f0fa00eb8577c98b119fac414cb1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-12 15:03:33 +0600

    added

---
+++ CodeForces/Q_Product.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int l, r, m;
+    cin >> l >> r >> m;
+    //  Print the product of all numbers from L to R (inclusive) module M
+    
+
+    return 0;
+}
