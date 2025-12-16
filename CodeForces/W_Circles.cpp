66442c17211be95b8bdd10799c23cf70919860a2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-16 10:04:43 +0600

    added

--- CodeForces/W_Circles.cpp
+++ CodeForces/W_Circles.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    int x3, y3, x4, y4;
+    cin >> x3 >> y3 >> x4 >> y4;
     
     return 0;
 }
