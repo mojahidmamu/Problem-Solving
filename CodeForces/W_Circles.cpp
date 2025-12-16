cfa437385464f5bbb2fd9727b6feaba31263aec3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-16 10:17:48 +0600

    added

--- CodeForces/W_Circles.cpp
+++ CodeForces/W_Circles.cpp
@@ -5,28 +5,29 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    int x1, y1, x2, y2;
+    long long x1, y1, x2, y2;
+    long long x3, y3, x4, y4;
+
     cin >> x1 >> y1 >> x2 >> y2;
-    int x3, y3, x4, y4;
     cin >> x3 >> y3 >> x4 >> y4;
 
-    double cx1 = (x1 + x2) / 2.0;
-    double cy1 = (y1 + y2) / 2.0;
-    double cx2 = (x3 + x4) / 2.0;
-    double cy2 = (y3 + y4) / 2.0;
+    // Diameter squared
+    long long d1 = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+    long long d2 = (x3 - x4) * (x3 - x4) + (y3 - y4) * (y3 - y4);
 
-    double r1 = sqrt((cx1 - x1) * (cx1 - x1) + (cy1 - y1) * (cy1 - y1));
-    double r2 = sqrt((cx2 - x3) * (cx2 - x3) + (cy2 - y3) * (cy2 - y3));
+    // Center distance squared * 4
+    long long dx = (x1 + x2) - (x3 + x4);
+    long long dy = (y1 + y2) - (y3 + y4);
+    long long dist4 = dx * dx + dy * dy;
 
-    double d = sqrt((cx1 - cx2) * (cx1 - cx2) + (cy1 - cy2) * (cy1 - cy2));
-    if (r1 + r2 >= d && abs(r1 - r2) <= d)
-    {
+    // (r1 + r2)^2 * 4
+    long long sum = d1 + d2 + 2 * sqrt((long double)d1 * d2);
+    long long diff = d1 + d2 - 2 * sqrt((long double)d1 * d2);
+
+    if (diff <= dist4 && dist4 <= sum)
         cout << "YES\n";
-    }
     else
-    {
         cout << "NO\n";
-    }
 
-        return 0;
+    return 0;
 }
