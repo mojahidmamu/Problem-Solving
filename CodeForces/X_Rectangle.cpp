3d3ae201691b22b91e75b7baea6e3366cc803413
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-18 07:55:18 +0600

    added

---
+++ CodeForces/X_Rectangle.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int x1, y1, x2, y2, x3, y3, x4, y4;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+    }
+
+    return 0;
+}
