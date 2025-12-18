8273c9fd7c45ca3cfa4a991ba9c1037fa57be898
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-18 08:02:46 +0600

    added

---
+++ CodeForces/Y_Common_Area.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            int x1, y1, x2, y2;
+            cin >> x1 >> y1 >> x2 >> y2;
+        }
+    }
+    return 0;
+}
