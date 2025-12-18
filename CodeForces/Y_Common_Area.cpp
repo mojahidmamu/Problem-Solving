22b04cb3ab6e92fb2fbf38ba661e590f8719ec48
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-18 08:08:15 +0600

    added

--- CodeForces/Y_Common_Area.cpp
+++ CodeForces/Y_Common_Area.cpp
@@ -11,11 +11,29 @@ int main()
     {
         int n;
         cin >> n;
+
+        int common_left = -100000;
+        int common_right = 100000;
+        int common_bottom = -100000;
+        int common_top = 100000;
+
         for (int i = 0; i < n; i++)
         {
             int x1, y1, x2, y2;
             cin >> x1 >> y1 >> x2 >> y2;
+
+            common_left = max(x1, common_left);
+            common_right = min(x1 + 10, common_right);
+            common_bottom = max(y1, common_bottom);
+            common_top = min(y1 + 10, common_top);
         }
+
+        int area = 0;
+        if (common_right > common_left && common_top > common_bottom)
+        {
+            area = (common_right - common_left) * (common_top - common_bottom);
+        }
+        cout << area << "\n";
     }
     return 0;
 }
