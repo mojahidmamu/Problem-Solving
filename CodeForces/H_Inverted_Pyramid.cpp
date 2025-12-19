da264bdd1137f2d1840530a3412a8d54cd33017c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-19 11:37:20 +0600

    added

--- CodeForces/H_Inverted_Pyramid.cpp
+++ CodeForces/H_Inverted_Pyramid.cpp
@@ -1,20 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+void pyramid(int n)
+{
+    for (int i = n; i >= 1; --i)
+    {
+        for (int j = 0; j < n - i; ++j)
+        {
+            cout << ' ';
+        }
+        for (int k = 0; k < 2 * i - 1; ++k)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
     int n;
     cin >> n;
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 0; j < n - i; ++j) {
-            cout << ' ';
-        }
-        for (int k = 0; k < 2 * i - 1; ++k) {
-            cout << '*';
-        }
-        cout << '\n';
-    }
-    
+
+    pyramid(n);
+
     return 0;
 }
