1bdbd893ce7dceeede39b0f19cdd5f1dcdfd8fec
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-20 21:57:05 +0600

    added

---
+++ CodeForces/Room Allocation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+    }
+
+    return 0;
+}
