fe2e1c00ac4817f6cbc1f83f09e2bb921ac36f22
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-21 07:18:28 +0600

    added

--- CodeForces/Shoot and kill.cpp
+++ CodeForces/Shoot and kill.cpp
@@ -1,9 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int x, y;
+        cin >> x >> y;
+    }
+
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int L, R;
+        cin >> L >> R;
+    }
+
     return 0;
 }
