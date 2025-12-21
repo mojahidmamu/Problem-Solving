e724d45373406e4bc2ae234c8a9531d0c874e45b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-21 07:51:20 +0600

    added

--- CodeForces/Shoot and kill.cpp
+++ CodeForces/Shoot and kill.cpp
@@ -4,6 +4,44 @@ using namespace std;
 #define ll long long
 #define endl '\n'
 
+struct SegmentTree
+{
+    int n;
+    vector<int> tree;
+
+    SegmentTree(vector<int> &a)
+    {
+        n = a.size();
+        tree.resize(4 * n);
+        build(1, 1, n, a);
+    }
+
+    void build(int node, int l, int r, vector<int> &a)
+    {
+        if (l == r)
+        {
+            tree[node] = a[l];
+            return;
+        }
+        int mid = (l + r) / 2;
+        build(node * 2, l, mid, a);
+        build(node * 2 + 1, mid + 1, r, a);
+        tree[node] = max(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    int query(int node, int l, int r, int ql, int qr)
+    {
+        if (r < ql || l > qr)
+            return 0;
+        if (ql <= l && r <= qr)
+            return tree[node];
+        int mid = (l + r) / 2;
+        return max(
+            query(node * 2, l, mid, ql, qr),
+            query(node * 2 + 1, mid + 1, r, ql, qr));
+    }
+};
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -17,16 +55,17 @@ int main()
     {
         cin >> range[i].first >> range[i].second;
         st.insert(range[i].first);
-        st.insert(range[i].second);
+        st.insert(range[i].second + 1);
     }
 
     int q;
     cin >> q;
-    vector<long long> queries(q);
+    vector<pair<long long, long long>> queries(q);
     for (int i = 0; i < q; i++)
     {
-        cin >> queries[i];
-        st.insert(queries[i]);
+        cin >> queries[i].first >> queries[i].second;
+        st.insert(queries[i].first);
+        st.insert(queries[i].second);
     }
 
     map<long long, int> compress;
@@ -35,6 +74,6 @@ int main()
     {
         compress[x] = cur++;
     }
-    
+
     return 0;
 }
