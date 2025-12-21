a515403ca598778363162b779c264d141f926f98
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-21 07:37:40 +0600

    added

--- CodeForces/Shoot and kill.cpp
+++ CodeForces/Shoot and kill.cpp
@@ -7,18 +7,29 @@ int main()
     // --- your code here ---
     int n;
     cin >> n;
-    while (n--)
+    vector<pair<long long, long long>> range(n);
+    set<long long> st;
+    for (int i = 0; i < n; i++)
     {
-        int x, y;
-        cin >> x >> y;
+        cin >> range[i].first >> range[i].second;
+        st.insert(range[i].first);
+        st.insert(range[i].second);
     }
-
+    
     int q;
     cin >> q;
-    while (q--)
+    vector<long long> queries(q);
+    for (int i = 0; i < q; i++)
     {
-        int L, R;
-        cin >> L >> R;
+        cin >> queries[i];
+        st.insert(queries[i]);
+    } 
+
+    map<long long, int> compress;
+    int cur = 1;
+    for (auto &x : st)
+    {
+        compress[x] = cur++;
     }
 
     return 0;
