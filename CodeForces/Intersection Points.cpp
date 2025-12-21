ae5795b4eb5c2fa01abb0962353b2f73473f9425
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-21 09:19:53 +0600

    added

--- CodeForces/Intersection Points.cpp
+++ CodeForces/Intersection Points.cpp
@@ -4,6 +4,15 @@ using namespace std;
 #define ll long long
 #define endl '\n'
 
+struct HorizontalLine
+{
+    int y, x1, x2;
+};
+struct VerticalLine
+{
+    int x, y1, y2;
+};
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -11,12 +20,36 @@ int main()
 
     int n;
     cin >> n;
+    vector<HorizontalLine> hLines;
+    vector<VerticalLine> vLines;
     for (int i = 0; i < n; i++)
     {
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
-        
+
+        if (y1 == y2)
+        {
+            hLines.push_back({y1, min(x1, x2), max(x1, x2)});
+        }
+        else if (x1 == x2)
+        {
+            vLines.push_back({x1, min(y1, y2), max(y1, y2)});
+        }
     }
+
+    int intersections = 0;
+    for(auto &h : hLines)
+    {
+        for(auto &v : vLines)
+        {
+            if(v.x >= h.x1 && v.x <= h.x2 && h.y >= v.y1 && h.y <= v.y2)
+            {
+                intersections++;
+            }
+        }
+    }
+
+    cout << intersections << endl;
 
     return 0;
 }
