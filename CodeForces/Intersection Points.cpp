0add1884138066b7f682251ce75271d8c7536bd4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-21 09:07:02 +0600

    added

--- CodeForces/Intersection Points.cpp
+++ CodeForces/Intersection Points.cpp
@@ -11,7 +11,12 @@ int main()
 
     int n;
     cin >> n;
-    
+    for (int i = 0; i < n; i++)
+    {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        
+    }
 
     return 0;
 }
