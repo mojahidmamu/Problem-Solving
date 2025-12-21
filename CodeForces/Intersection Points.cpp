1b08ad952ed959636de1a0f35660acec8deefe3b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-21 08:01:21 +0600

    added

---
+++ CodeForces/Intersection Points.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    
+
+    return 0;
+}
