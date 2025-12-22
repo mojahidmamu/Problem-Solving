0e1915c783dbfa4fad1e46cfafad681c26929954
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-22 09:16:44 +0600

    added

--- CodeForces/O_Fibonacci.cpp
+++ CodeForces/O_Fibonacci.cpp
@@ -1,5 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int fibo(int n)
+{
+    if (n == 1)
+    {
+       return 0;
+    }
+    if (n == 2)
+    {
+        return 1;
+    }
+    return fibo(n - 1) + fibo(n - 2);
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -7,27 +21,19 @@ int main()
     // --- your code here ---
     long long n;
     cin >> n;
-    if (n == 1)
-    {
-        cout << 0 << endl;
-        return 0;
-    }
-    if (n == 2)
-    {
-        cout << 1 << endl;
-        return 0;
-    }
 
-    long long prev2 = 0;
-    long long prev1 = 1;
-    long long cur = 0;
-    for (long long i = 3; i <= n; i++)
-    {
-        cur = prev2 + prev1;
-        prev2 = prev1;
-        prev1 = cur;
-    }
-    cout << cur << endl;
+    cout << fibo(n) << endl;
+
+    // long long prev2 = 0;
+    // long long prev1 = 1;
+    // long long cur = 0;
+    // for (long long i = 3; i <= n; i++)
+    // {
+    //     cur = prev2 + prev1;
+    //     prev2 = prev1;
+    //     prev1 = cur;
+    // }
+    // cout << cur << endl;
 
     return 0;
 }
