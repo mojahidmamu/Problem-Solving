a533ac12212a59027b00e112279fd481622ecf18
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-24 21:25:20 +0600

    added

---
+++ CodeForces/Y_Number_of_Ways.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int count(int s, int e) {
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int s, e;
+    cin >> s >> e;
+    cout << count(s, e) << endl;
+    return 0;
+}
