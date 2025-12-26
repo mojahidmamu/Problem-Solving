2a25826802130bd6503aebbeab4d4bcb1c96d030
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-26 09:36:40 +0600

    added

---
+++ CodeForces/Z_Left_Max.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    return 0;
+}
