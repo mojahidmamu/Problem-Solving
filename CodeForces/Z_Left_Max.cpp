228c5f061b4406c34a871b88c486ba9947d7aa31
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-26 09:47:50 +0600

    added

--- CodeForces/Z_Left_Max.cpp
+++ CodeForces/Z_Left_Max.cpp
@@ -4,8 +4,17 @@ using namespace std;
 #define ll long long
 #define endl '\n'
 
+int n;
+long long arr[100005];
+
 long long MaxTill(int i)
 {
+    if (i == 0)
+    {
+        return arr[0];
+    }
+    long long premax = MaxTill(i - 1);
+    return max(premax, arr[i - 1]);
 }
 
 int main()
@@ -13,9 +22,7 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
     cin >> n;
-    int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
