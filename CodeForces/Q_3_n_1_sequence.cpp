84a55ab738400bbdbe44505b978d912a892ea70d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-28 05:05:08 +0600

    added

---
+++ CodeForces/Q_3_n_1_sequence.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < 3 * n + 1; i++)
+    {
+        /* code */
+    }
+
+    return 0;
+}
