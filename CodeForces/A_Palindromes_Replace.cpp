b6a2ca2f18912f3ae24c5e99a4ef57c66f961c46
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-28 05:30:47 +0600

    added

--- CodeForces/A_Palindromes_Replace.cpp
+++ CodeForces/A_Palindromes_Replace.cpp
@@ -11,7 +11,39 @@ int main()
 
     string s;
     cin >> s;
-    
+    int n = s.size();
+
+    for (int i = 0; i < n / 2; i++)
+    {
+        int j = n - 1 - i;
+        if (s[i] != '?' && s[j] != '?')
+        {
+            if (s[i] != s[j])
+            {
+                cout << -1 << endl;
+                return 0;
+            }
+        }
+        else if (s[i] == '?' && s[j] == '?')
+        {
+            s[i] = s[j] = 'a';
+        }
+        else if (s[i] == '?')
+        {
+            s[i] = s[j];
+        }
+        else
+        {
+            s[j] = s[i];
+        }
+    }
+
+    if (n % 2 == 1 && s[n / 2] == '?')
+    {
+        s[n / 2] = 'a';
+    }
+
+    cout << s << endl;
 
     return 0;
 }
