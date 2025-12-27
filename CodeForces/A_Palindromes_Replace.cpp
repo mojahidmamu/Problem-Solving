4d0aaa1670b3cf0a1f8938654885ac698a6dc6a9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-28 05:14:46 +0600

    added

---
+++ CodeForces/A_Palindromes_Replace.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+    
+
+    return 0;
+}
