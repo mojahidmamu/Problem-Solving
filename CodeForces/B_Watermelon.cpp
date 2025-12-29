4b5b57971a27b28af9c2a28966560dd219d5278f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-29 08:36:08 +0600

    added

---
+++ CodeForces/B_Watermelon.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int w;
+    cin >> w;
+    
+
+    return 0;
+}
