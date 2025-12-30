618d8079ce46b0c322fb1c97ec579613523112b8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-30 20:34:52 +0600

    added

--- HackerRank/Railway Platform.cpp
+++ HackerRank/Railway Platform.cpp
@@ -4,6 +4,32 @@ using namespace std;
 #define ll long long
 #define endl '\n'
 
+struct FenwickTree
+{
+    vector<int> bit;
+    int n;
+
+    FenwickTree(int size)
+    {
+        n = size;
+        bit.assign(n + 1, 0);
+    }
+
+    void update(int index, int val)
+    {
+        for (++index; index <= n; index += index & -index)
+            bit[index] += val;
+    }
+
+    int query(int index)
+    {
+        int sum = 0;
+        for (++index; index > 0; index -= index & -index)
+            sum += bit[index];
+        return sum;
+    }
+};
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -17,20 +43,26 @@ int main()
         cin >> n;
         vector<int> arr(n);
         for (int i = 0; i < n; i++)
-        {
             cin >> arr[i];
+
+        vector<int> sorted_arr = arr;
+        sort(sorted_arr.begin(), sorted_arr.end());
+        map<int, int> compress;
+        int idx = 0;
+        for (int x : sorted_arr)
+        {
+            if (!compress.count(x))
+                compress[x] = idx++;
         }
 
+        FenwickTree ft(idx);
         ll count = 0;
-        for (int i = 0; i < n; i++)
+
+        for (int i = n - 1; i >= 0; i--)
         {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (arr[i] >= arr[j])
-                {
-                    count++;
-                }
-            }
+            int comp_val = compress[arr[i]];
+            count += ft.query(comp_val);
+            ft.update(comp_val, 1);
         }
 
         cout << count << endl;
