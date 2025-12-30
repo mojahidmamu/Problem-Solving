bec5a25570503f8ee8cb6d5575a862146739c005
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-30 20:25:39 +0600

    added

---
+++ HackerRank/Xorry Pairs.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        if (n == 0)
+        {
+            cout << 1 << endl;
+            continue;
+        }
+        int ones = __builtin_popcountll(n);
+        cout << (1ll << (ones - 1)) << endl;
+    }
+
+    return 0;
+}
