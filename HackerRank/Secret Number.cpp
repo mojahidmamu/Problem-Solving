df42849284664eef4a5ed295cd51cff2d0438c3e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-30 20:02:52 +0600

    added

---
+++ HackerRank/Secret Number.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n;
+    cin >> n;
+    cout << abs(n) % 10 << endl;
+    
+    return 0;
+}
