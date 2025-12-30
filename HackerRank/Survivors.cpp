84ba3fe171b1cce59d7d50b99b7f2c579e30c137
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-30 20:20:41 +0600

    added

--- HackerRank/Survivors.cpp
+++ HackerRank/Survivors.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std; 
 
 #define ll long long
 #define endl '\n'
@@ -9,13 +9,47 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, t;
+    int n;
+    ll  t;
     cin >> n >> t;
-    int arr[n];
+    vector<ll> arr(n);
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
+    ll low = 0, high = 1e18;
+    ll ans = 0;
+    while (low <= high)
+    {
+        ll mid = (low + high) / 2;
+        ll need = 0;
+        bool overflow = false;
+
+        for (int i = 0; i < n; i++)
+        {
+            ll gaps = (t - 1) / arr[i];
+            ll times = (mid + gaps) / (gaps + 1);
+            need += times;
+
+            if (need > mid)
+            {
+                overflow = true;
+                break;
+            }
+        }
+
+        if (!overflow)
+        {
+            ans = mid;
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    cout << ans << endl;
     return 0;
 }
