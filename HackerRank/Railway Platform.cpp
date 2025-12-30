901f2f94f1ab3e46482482062aa51ed141058b6a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-30 20:31:14 +0600

    added

--- HackerRank/Railway Platform.cpp
+++ HackerRank/Railway Platform.cpp
@@ -11,17 +11,29 @@ int main()
 
     int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
         int n;
         cin >> n;
         vector<int> arr(n);
         for (int i = 0; i < n; i++)
         {
-            cin>> arr[i];
+            cin >> arr[i];
         }
-        
-        
+
+        ll count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[i] >= arr[j])
+                {
+                    count++;
+                }
+            }
+        }
+
+        cout << count << endl;
     }
 
     return 0;
