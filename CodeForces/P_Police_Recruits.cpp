ca3ba1f79b3fb91e383a908a90f83f49d6556914
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-31 20:49:12 +0600

    added

--- CodeForces/P_Police_Recruits.cpp
+++ CodeForces/P_Police_Recruits.cpp
@@ -14,11 +14,27 @@ int main()
 
     int available = 0;
     int unrated = 0;
-     
+
     for (int i = 0; i < n; i++)
     {
-         int event;
-         cin >> event;  
+        int event;
+        cin >> event;
+
+        if (event == -1)
+        {
+            if (available > 0)
+            {
+                available++;
+            }
+            else
+            {
+                unrated++;
+            }
+        }
+        else
+        {
+            available += event;
+        }
     }
 
     cout << unrated << endl;
