9919f0be609484321c50f4c4f123e81e7372aefd
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-31 19:30:46 +0600

    added

--- CodeForces/K_Prime_Fibonacci.cpp
+++ CodeForces/K_Prime_Fibonacci.cpp
@@ -6,6 +6,18 @@ using namespace std;
 
 bool isPrime(ll n)
 {
+    if (n < 2)
+    {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main()
