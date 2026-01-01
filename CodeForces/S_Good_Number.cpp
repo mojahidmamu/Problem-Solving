2e5787f13d04af0cb26df28f651966f7499753db
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-01 20:18:03 +0600

    added

---
+++ CodeForces/S_Good_Number.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+    while (n--)
+    {
+        int a;
+        cin >> a;
+    }
+    
+
+    return 0;
+}
