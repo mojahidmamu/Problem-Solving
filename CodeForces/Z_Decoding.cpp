3df1fa8be4453982e04cb133c9e799550e295837
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-04 12:27:48 +0600

    added

--- CodeForces/Z_Decoding.cpp
+++ CodeForces/Z_Decoding.cpp
@@ -13,7 +13,30 @@ int main()
     cin >> n;
     string s;
     cin >> s;
-    
+    string res;
+    for (int i = 0; i < n;)
+    {
+        if (s[i] == '0')
+        {
+            res += '0';
+            i++;
+        }
+        else if (s[i] == '1')
+        {
+            if (s[i + 1] == '0')
+            {
+                res += '1';
+                i += 2;
+            }
+            else
+            {
+                int num = (s[i + 1] - '0') * 10 + (s[i + 2] - '0');
+                res += char('a' + num - 1);
+                i += 3;
+            }
+        }
+    }
+    cout << res << endl;
 
     return 0;
 }
