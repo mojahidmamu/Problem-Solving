1d130dfaead6ab45b7eddcdf5f139f8f86693215
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-06 14:41:32 +0600

    added

--- CodeForces/Y_Team_Olympiad.cpp
+++ CodeForces/Y_Team_Olympiad.cpp
@@ -11,13 +11,33 @@ int main()
 
     int n;
     cin >> n;
-    int arr[n];
+    vector<int> programmer, math, PE;
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        int t;
+        cin >> t;
+
+        if (t == 1)
+        {
+            programmer.push_back(i + 1);
+        }
+        else if (t == 2)
+        {
+            math.push_back(i + 1);
+        }
+        else if (t == 3)
+        {
+            PE.push_back(i + 1);
+        }
     }
-    
-    
+
+    int w = min({programmer.size(), math.size(), PE.size()});
+    cout << w << endl;
+
+    for (int i = 0; i < w; i++)
+    {
+        cout << programmer[i] << " " << math[i] << " " << PE[i] << endl;
+    }
 
     return 0;
 }
