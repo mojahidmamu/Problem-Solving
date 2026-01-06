51a6751ed51447b7cdcf3171585a5a5318c95adc
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-06 14:20:09 +0600

    added

--- CodeForces/Y_Team_Olympiad.cpp
+++ CodeForces/Y_Team_Olympiad.cpp
@@ -9,12 +9,15 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t;
-    cin >> t;
-    while(t--)
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
     {
-        // solve one test case
+        cin >> arr[i];
     }
+    
+    
 
     return 0;
 }
