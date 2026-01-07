4647a1775c95bc727e59715f25e68c9f90074117
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-07 20:37:50 +0600

    added

---
+++ CodeForces/A_Binary_Array_Game.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+    }
+
+    return 0;
+}
