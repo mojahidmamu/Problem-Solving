268122fbada3b7d9e1fade9c65aba39dc953a107
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-12 20:59:36 +0600

    mojahidMamu

---
+++ CodeForces/E_Exquisite_Array.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr.begin(), arr.end());
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << ' ';
+        }
+        cout << endl;
+        }
+
+    return 0;
+}
