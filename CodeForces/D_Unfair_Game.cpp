3b610c6abaf0084f10c2c39879590070d0596695
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-12 21:09:22 +0600

    mojahidMamu

--- CodeForces/D_Unfair_Game.cpp
+++ CodeForces/D_Unfair_Game.cpp
@@ -1,37 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define ll long long
-#define endl '\n'
 
-int main()
-{
+ll solve(ll n, ll k) {
+    ll res = 0;
+    ll moves = 0;
+    ll cur = 1;
+    while (cur <= n && moves < k) {
+        if (cur % 2 == 1) {
+            cur = cur * 2;
+        } else {
+            cur = cur * 2 - 1;
+        }
+        moves++;
+    }
+    res = n - cur + 1;
+    if (res < 0) res = 0;
+    return res;
+}
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         ll n, k;
         cin >> n >> k;
+        ll ans = n;
 
-        long long d = 0, temp = n;
-        while (temp > 1)
-        {
-            temp /= 2;
-            d++;
+        ll low = 1, high = n;
+        while (low <= high) {
+            ll mid = (low + high) / 2;
+            ll x = mid, moves = 0;
+            while (x) {
+                if (x % 2 == 0) x /= 2;
+                else x--;
+                moves++;
+            }
+            if (moves > k) high = mid - 1;
+            else low = mid + 1;
         }
-
-        if (k >= d)
-        {
-            cout << 0 << "\n";
-        }
-        else
-        {
-            long long safe = n - ((1LL << k) - 1);
-            cout << safe << "\n";
-        }
+        cout << n - high << endl;
     }
 
     return 0;
