031d0d142b8a2446c3cdfc50bab82280af28f499
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-12 21:04:48 +0600

    mojahidMamu

--- CodeForces/D_Unfair_Game.cpp
+++ CodeForces/D_Unfair_Game.cpp
@@ -11,15 +11,27 @@ int main()
 
     int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
         ll n, k;
         cin >> n >> k;
-        ll full_cycles = n / (2 * k);
-        ll remainder = n % (2 * k);
-        ll result = full_cycles * k + min(remainder, k);
-        cout << result << endl;
-        
+
+        long long d = 0, temp = n;
+        while (temp > 1)
+        {
+            temp /= 2;
+            d++;
+        }
+
+        if (k >= d)
+        {
+            cout << 0 << "\n";
+        }
+        else
+        {
+            long long safe = n - ((1LL << k) - 1);
+            cout << safe << "\n";
+        }
     }
 
     return 0;
