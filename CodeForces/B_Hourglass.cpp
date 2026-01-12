fb754b0341064e043ad0c246e0cff357c3494917
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-12 20:39:07 +0600

    mojahidMamu

---
+++ CodeForces/B_Hourglass.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+         ll s, k, m;
+         cin >> s >> k >> m;
+         
+    }
+
+    return 0;
+}
