afd4a5be1736f9eff84a40faef9558b1116a1d6e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-18 20:57:33 +0600

    mojahidMamu

---
+++ CodeForces/D_OutOfMemoryError.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, m, h;
+        cin >> n >> m >> h;
+        vector<ll> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        while (m--)
+        {
+            ll b, c;
+            cin >> b >> c;
+            
+        }
+        
+    }
+
+    return 0;
+}
