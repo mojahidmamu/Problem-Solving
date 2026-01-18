2615bc2100e5e91c687ce1ed443717dad17f931b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-18 20:46:33 +0600

    mojahidMamu

--- CodeForces/B_Prefix_Max.cpp
+++ CodeForces/B_Prefix_Max.cpp
@@ -4,6 +4,18 @@ using namespace std;
 #define ll long long
 #define endl '\n'
 
+long long valueOfArray(const vector<int> &a)
+{
+    long long sum = 0;
+    int currentMax = 0;
+    for (int x : a)
+    {
+        currentMax = max(currentMax, x);
+        sum += currentMax;
+    }
+    return sum;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -11,22 +23,29 @@ int main()
 
     int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
         int n;
         cin >> n;
         vector<int> a(n);
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
             cin >> a[i];
         }
-        sort(a.begin(), a.end());
-        for(int i = 0; i < n; i++)
+
+        long long ans = valueOfArray(a);
+
+        for (int i = 0; i < n; i++)
         {
-            cout << a[i] << " ";
+            for (int j = i + 1; j < n; j++)
+            {
+                swap(a[i], a[j]);
+                ans = max(ans, valueOfArray(a));
+                swap(a[i], a[j]);  
+            }
         }
-        cout << '\n';
-        
+
+        cout << ans << '\n';
     }
 
     return 0;
