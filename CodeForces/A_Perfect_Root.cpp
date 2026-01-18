46ec522c316acadea32ebbba2ede06acef6e4eb9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-18 20:37:03 +0600

    mojahidMamu

--- CodeForces/A_Perfect_Root.cpp
+++ CodeForces/A_Perfect_Root.cpp
@@ -11,12 +11,15 @@ int main()
 
     int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
         int n;
         cin >> n;
-
-        
+        for (int i = 1; i <= n; i++)
+        {
+            cout << i << " ";
+        }
+        cout << "\n";
     }
 
     return 0;
