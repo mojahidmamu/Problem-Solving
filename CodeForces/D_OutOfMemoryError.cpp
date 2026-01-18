60411bb5a68617bc59d64f5e2411f6b44513b13b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-18 21:32:06 +0600

    mojahidMamu

--- CodeForces/D_OutOfMemoryError.cpp
+++ CodeForces/D_OutOfMemoryError.cpp
@@ -15,18 +15,60 @@ int main()
     {
         ll n, m, h;
         cin >> n >> m >> h;
-        vector<ll> a(n);
+        
+        vector<long long> orig(n), add(n, 0);
+        vector<int> last_update(n, -1);
+
         for (int i = 0; i < n; i++)
         {
-            cin >> a[i];
+            cin >> orig[i];
         }
+
+        int reset_id = 0;
+
         while (m--)
         {
             ll b, c;
             cin >> b >> c;
-            
+            b--;
+            long long current;
+            if (last_update[b] < reset_id)
+            {
+                current = orig[b];
+            }
+            else
+            {
+                current = orig[b] + add[b];
+            }
+
+            if (current + c > h)
+            {
+                reset_id++;
+            }
+            else
+            {
+                if (last_update[b] < reset_id)
+                {
+                    add[b] = 0;
+                    last_update[b] = reset_id;
+                }
+                add[b] += c;
+            }
         }
-        
+
+
+        for (int i = 0; i < n; i++)
+        {
+            if (last_update[i] < reset_id)
+            {
+                cout << orig[i] << " ";
+            }
+            else
+            {
+                cout << orig[i] + add[i] << " ";
+            }
+        }
+        cout << "\n";
     }
 
     return 0;
