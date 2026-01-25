ae8c79618d299fd1b0a6215f184c3a9ab8829f81
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-25 20:47:51 +0600

    mojahidMamu

--- CodeForces/C_Replace_and_Sum.cpp
+++ CodeForces/C_Replace_and_Sum.cpp
@@ -11,10 +11,26 @@ int main()
 
     int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
-        // solve one test case
-    }
+        int n, q;
+        cin >> n >> q;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        vector<int> b(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> b[i];
+        }
+        while (q--)
+        {
+            int l, r;
+            cin >> l >> r;
+        }
+        }
 
     return 0;
 }
