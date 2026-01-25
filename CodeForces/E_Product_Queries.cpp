22aeebf1e23ce2ec451919800a00415cd0bf064e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-25 21:37:27 +0600

    mojahidMamu

--- CodeForces/E_Product_Queries.cpp
+++ CodeForces/E_Product_Queries.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #define ll long long
 #define endl '\n'
 
+const int INF = 1e9;
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -15,11 +17,53 @@ int main()
     {
         int n;
         cin >> n;
+
         vector<int> a(n);
+        vector<int> exists(n + 1, 0);
         for (int i = 0; i < n; i++)
         {
             cin >> a[i];
+            exists[a[i]] = 1;
         }
+
+        vector<int> dp(n + 1, INF);
+
+        for (int x = 1; x <= n; x++)
+        {
+            if (exists[x])
+            {
+                dp[x] = 1;
+            }
+        }
+
+        for (int v = 1; v <= n; v++)
+        {
+            if (!exists[v])
+            {
+                continue;
+            }
+
+            for (int x = v; x <= n; x += v)
+            {
+                if (dp[x / v] != INF)
+                {
+                    dp[x] = min(dp[x], dp[x / v] + 1);
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (dp[i] == INF)
+            {
+                cout << -1 << " ";
+            }
+            else
+            {
+                cout << dp[i] << " ";
+            }
+        }
+        cout << "\n";
     }
 
     return 0;
