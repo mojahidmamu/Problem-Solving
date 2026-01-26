628ee425766f17f851b48cc5f6162522f83be6c6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-26 09:12:39 +0600

    mojahidMamu

--- CodeForces/D_Monster_Game.cpp
+++ CodeForces/D_Monster_Game.cpp
@@ -11,9 +11,15 @@ int main()
 
     int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
-        // solve one test case
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
     }
 
     return 0;
