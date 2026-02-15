ad70c9b97f2c0de89d3a35f5c38be658f0fa1b89
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-15 20:47:40 +0600

    added

--- CodeForces/A_Sieve_of_Erato_67_henes.cpp
+++ CodeForces/A_Sieve_of_Erato_67_henes.cpp
@@ -63,8 +63,27 @@ int main()
     cin >> t;
     while (t--)
     {
-        // write code here...
-    }
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(all(arr));
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (gcd(arr[i], arr[j]) == 1)
+                {
+                    count++;
+                }
+            }
+        }
+        cout << count << endl;
+        }
 
     return 0;
 }
