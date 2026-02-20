4947c1900caed167c934736fd3c7fa420b998581
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-20 14:45:16 +0600

    added

--- HackerRank/03.cpp
+++ HackerRank/03.cpp
@@ -1,56 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-
-// Author: Abdullah all Mojahid
-
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n;
     cin >> n;
     vector<int> arr(n);
-    unordered_map<int, int> freq;
+    int max_val = 0;
 
-    for (int i = 0; i < n; i++)
-    {
+    for(int i = 0; i < n; i++) {
         cin >> arr[i];
-        freq[arr[i]]++;
-    }
-
-    int mex = 0;
-    while (freq[mex] > 0)
-    {
-        mex++;
+        max_val = max(max_val, arr[i]);
     }
 
     int q;
     cin >> q;
-    while (q--)
-    {
+
+    // Size freq array: max_val + q + 5
+    vector<int> freq(max_val + q + 5, 0);
+    for(int i = 0; i < n; i++) freq[arr[i]]++;
+
+    // Initial Mex
+    int mex = 0;
+    while(freq[mex] > 0) mex++;
+
+    while(q--) {
         int x;
         cin >> x;
-        if (freq[x] == 0)
-        {
+        if(freq[x] == 0) {
             cout << -1 << "\n";
-        }
-        else
-        {
+        } else {
+            // Remove one occurrence
             freq[x]--;
 
-            // recompute mex
-            int curr_mex = 0;
-            while (freq[curr_mex] > 0)
-            {
-                curr_mex++;
+            int curr_mex = mex;
+            if(x == mex) {
+                while(freq[curr_mex] > 0) curr_mex++;
             }
-
             cout << curr_mex << "\n";
 
-            // restore x
+            // Restore
             freq[x]++;
         }
     }
--- HackerRank/04.cpp
+++ HackerRank/04.cpp
@@ -1,23 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define endl '\n'
-#define all(x) (x).begin(), (x).end()
-
-// Author: Abdullah all Mojahid
-
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t;
-    cin >> t;
-    while (t--)
+    int T;
+    cin >> T;
+    while (T--)
     {
-        // write code here...
-    }
+        int x1, y1, x2, y2, x3, y3;
+        cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+        // Check collinear
+        long long area2 = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+        if (area2 != 0)
+        {
+            cout << "YES\n"; // Not collinear → circle exists
+            continue;
+        }
 
+        // Check center condition
+        auto sq = [](long long dx, long long dy)
+        { return dx * dx + dy * dy; };
+        if (sq(x1 - x2, y1 - y2) == sq(x1 - x3, y1 - y3) ||
+            sq(x2 - x1, y2 - y1) == sq(x2 - x3, y2 - y3) ||
+            sq(x3 - x1, y3 - y1) == sq(x3 - x2, y3 - y2))
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
     return 0;
 }
