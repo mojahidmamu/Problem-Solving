39d383a784d408099ee48423ba833b9a23566014
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-20 13:58:05 +0600

    added

---
+++ HackerRank/01.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        // write code here...
+    }
+
+    return 0;
+}
