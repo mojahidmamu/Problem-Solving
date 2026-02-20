be49f65d82216a0e35dce117dfbabb998f40fb31
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-20 14:15:40 +0600

    added

--- HackerRank/01.cpp
+++ HackerRank/01.cpp
@@ -1,22 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define endl '\n'
-#define all(x) (x).begin(), (x).end()
-
-// Author: Abdullah all Mojahid
-
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t;
-    cin >> t;
-    while (t--)
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    bool isPresent = false;
+    for (int i = 0; i < n; i++)
     {
-        // write code here...
+        if (s[i] == 'B')
+        {
+            isPresent = true;
+            break;
+        }
+    }
+
+    if (isPresent )
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
     }
 
     return 0;
