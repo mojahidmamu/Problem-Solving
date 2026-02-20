6697c6f1df59e38dc88f51ec5f360b76f9d48eda
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-20 15:06:47 +0600

    added

--- HackerRank/07.cpp
+++ HackerRank/07.cpp
@@ -17,10 +17,8 @@ int main()
     }
 
     map<int, int> freq;
-    for (int value : arr)
-    {
-        freq[value]++;
-    }
+    for (int a : arr)
+        freq[a]++;
 
     int mexB = 0;
     while (true)
@@ -34,6 +32,33 @@ int main()
         {
             break;
         }
+
+        // Build remaining elements for S2
+        vector<int> remaining;
+        for (auto it : freq)
+        {
+            int val = it.first;
+            int cnt = it.second;
+            for (int i = 0; i < cnt; i++)
+                remaining.push_back(val);
+        }
+
+        // Subset sum DP to check if sum = mexB is possible
+        int target = mexB;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        for (int val : remaining)
+        {
+            for (int s = target; s >= val; s--)
+            {
+                if (dp[s - val])
+                    dp[s] = true;
+            }
+        }
+
+        if (!dp[target])
+            break; // cannot form sum = mexB
+        mexB++;
     }
 
     cout << mexB << endl;
