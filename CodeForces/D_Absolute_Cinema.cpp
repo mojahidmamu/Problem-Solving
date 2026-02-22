55834ab93d390947f8f737b1e2cd08aecc40722b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-22 07:25:26 +0600

    added

--- CodeForces/D_Absolute_Cinema.cpp
+++ CodeForces/D_Absolute_Cinema.cpp
@@ -14,30 +14,48 @@ int main()
         cin >> n;
         vector<long long> f(n);
         for (int i = 0; i < n; i++)
+        {
             cin >> f[i];
-        vector<long long> a(n);
-
-        if (n == 2)
-        {
-            a[0] = f[1];
-            a[1] = f[0];
-        }
-        else
-        {
-            // Compute a[0] = f[1] - f[0]
-            a[0] = f[1] - f[0];
-
-            // Compute rest a[i] using differences
-            for (int i = 1; i < n; i++)
-            {
-                a[i] = f[i] - f[i - 1] - a[i - 1];
-            }
         }
 
-        for (int i = 0; i < n; i++)
+        vector<long long> a(n + 1);
+        for (int i = 2; i <= n; i++)
         {
-            cout << a[i] << (i == n - 1 ? '\n' : ' ');
+            a[i] = (f[i] - 2ll * f[i - 1] - f[i - 2]) / 2;
         }
+
+        // a[1]
+        long long s = 0;
+        for (int i = 2; i <= n; ++i)
+        {
+            s += a[i] * (i - 1LL);
+        }
+        a[1] = f[1] - s;
+
+        // if (n == 2)
+        // {
+        //     a[0] = f[1];
+        //     a[1] = f[0];
+        // }
+        // else
+        // {
+        //     // Compute a[0] = f[1] - f[0]
+        //     a[0] = f[1] - f[0];
+
+        //     // Compute rest a[i] using differences
+        //     for (int i = 1; i < n; i++)
+        //     {
+        //         a[i] = f[i] - f[i - 1] - a[i - 1];
+        //     }
+        // }
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i > 1)
+                cout << " ";
+            cout << a[i];
+        }
+        cout << '\n';
     }
     return 0;
 }
