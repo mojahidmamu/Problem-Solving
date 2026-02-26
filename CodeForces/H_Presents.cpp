f07cce13256be64003c9c74a30dc574978c00f92
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-27 00:54:26 +0600

    added

---
+++ CodeForces/H_Presents.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        
+    }
+
+    return 0;
+}
