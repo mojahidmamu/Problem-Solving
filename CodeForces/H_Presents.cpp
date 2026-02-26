179237a690b45007470fd602be319eeccd8d6d74
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-27 01:04:13 +0600

    added

--- CodeForces/H_Presents.cpp
+++ CodeForces/H_Presents.cpp
@@ -14,12 +14,23 @@ int main()
 
     int n;
     cin >> n;
-    for (int i = 0; i < n; i++)
+    vector<int> a(n + 1);
+    vector<int> b(n + 1);
+    for (int i = 1; i <= n; i++)
     {
-        int a;
-        cin >> a;
-        
+        cin >> a[i];
     }
 
-    return 0;
+    for (int i = 1; i <= n; i++)
+    {
+        b[a[i]] = i;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << b[i] << " ";
+    }
+    cout << endl;
+
+        return 0;
 }
