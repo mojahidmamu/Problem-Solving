5b169b42a7518bc5ef640b02fa384543529f6ccf
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-27 01:06:10 +0600

    added

---
+++ CodeForces/I_Colorful_Stones_Simplified_Edition.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s, t;
+    cin >> s >> t;
+    
+
+    return 0;
+}
