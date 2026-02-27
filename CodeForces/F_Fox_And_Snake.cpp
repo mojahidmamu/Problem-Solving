a4294fdfd66ab0eed72427306f616a5b63d960e2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-28 02:52:09 +0600

    added

--- CodeForces/F_Fox_And_Snake.cpp
+++ CodeForces/F_Fox_And_Snake.cpp
@@ -14,26 +14,36 @@ int main()
 
     int n, m;
     cin >> n >> m;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j < m; j++)
+        if (i % 2 == 1) // odd row
         {
-            if (i % 2 == 0)
+            for (int j = 0; j < m; j++)
             {
                 cout << "#";
             }
-            else
+        }
+        else
+        {
+            if (i % 4 == 2) // right side
             {
-                if (j == 0 || j == m - 1)
-                {
-                    cout << "#";
-                }
-                else
+                for (int j = 0; j < m - 1; j++)
                 {
                     cout << ".";
                 }
+                cout << "#";
             }
+            else
+                /
+                {
+                    cout << "#";
+                    for (int j = 0; j < m - 1; j++)
+                    {
+                        cout << ".";
+                    }
+                }
         }
+
         cout << endl;
     }
 
