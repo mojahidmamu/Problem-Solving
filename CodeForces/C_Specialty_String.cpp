40d988442f1f584ae42050059f33dd439653dabc
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-27 21:01:21 +0600

    added

--- CodeForces/C_Specialty_String.cpp
+++ CodeForces/C_Specialty_String.cpp
@@ -20,7 +20,41 @@ int main()
         cin >> n;
         string s;
         cin >> s;
-        
+
+        if (n % 2)
+        {
+            cout << "NO\n";
+            continue;
+        }
+
+        vector<int> freq(26, 0);
+
+        for (char c : s)
+        {
+            freq[c - 'a']++;
+        }
+
+        stack<char> st;
+        for (char ch : s)
+        {
+            if (st.empty() || st.top() != ch)
+            {
+                st.push(ch);
+            }
+            else
+            {
+                st.pop();
+            }
+        }
+
+        if (st.empty())
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
     }
 
     return 0;
