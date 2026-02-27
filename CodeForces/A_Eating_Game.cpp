b256d188005216222bf9267262d60a5d2e569179
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-27 20:43:19 +0600

    added

--- CodeForces/A_Eating_Game.cpp
+++ CodeForces/A_Eating_Game.cpp
@@ -20,10 +20,44 @@ int main()
         cin >> n;
         vector<int> a(n);
         for (int i = 0; i < n; i++)
-         {
+        {
             cin >> a[i];
-         }
-         
+        }
+
+        set<int> winners;
+        for (int i = 0; i < n; i++)
+        {
+            vector<int> temp = a;
+            int cur = i;
+            int last = -1;
+
+            while (true)
+            {
+                bool done = true;
+                for (int x : temp)
+                {
+                    if (x > 0)
+                    {
+                        done = false;
+                    }
+                }
+
+                if (done)
+                {
+                    break;
+                }
+
+                if (temp[cur] > 0)
+                {
+                    temp[cur]--;
+                    last = cur;
+                }
+
+                cur = (cur + 1) % n;
+            }
+            winners.insert(last);
+        }
+        cout << winners.size() << endl;
     }
 
     return 0;
