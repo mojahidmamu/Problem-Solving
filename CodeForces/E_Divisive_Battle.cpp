d94a714a83f33e15420aca9cfe3c39ffa3defa8e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-27 21:40:13 +0600

    added

--- CodeForces/E_Divisive_Battle.cpp
+++ CodeForces/E_Divisive_Battle.cpp
@@ -5,37 +5,51 @@ using namespace std;
 #define endl '\n'
 #define all(x) (x).begin(), (x).end()
 
-int main() {
+// Author: Abdullah all Mojahid
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int n;
         cin >> n;
         vector<int> a(n);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             cin >> a[i];
         }
 
-        // Find first position where array is not non-decreasing
-        int first_bad = -1;
-        for (int i = 0; i < n - 1; i++) {
-            if (a[i] > a[i + 1]) {
-                first_bad = i;
+        bool non_decreasing = true;
+        int first_dec_index = -1;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (a[i] > a[i + 1])
+            {
+                non_decreasing = false;
+                first_dec_index = i;
                 break;
             }
         }
+        
 
-        if (first_bad == -1) {
-            // Already non-decreasing
+        if (non_decreasing)
+        {
             cout << "Bob\n";
-        } else {
-            // Alice wins if first_bad is even, Bob wins if odd
-            if (first_bad % 2 == 0) {
+        }
+        else
+        {
+            if (first_dec_index % 2 == 0)
+            {
                 cout << "Alice\n";
-            } else {
+            }
+            else
+            {
                 cout << "Bob\n";
             }
         }
