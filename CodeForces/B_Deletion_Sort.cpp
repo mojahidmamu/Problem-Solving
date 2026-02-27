80ab5b0055d8655a7c069bf3f92a6953a777d36f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-27 20:46:28 +0600

    added

--- CodeForces/B_Deletion_Sort.cpp
+++ CodeForces/B_Deletion_Sort.cpp
@@ -16,7 +16,33 @@ int main()
     cin >> t;
     while (t--)
     {
-        // write code here...
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        vector<int> dp(n, 1);
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (a[j] <= a[i])
+                {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        for (int x : dp)
+        {
+            ans = max(ans, x);
+        }
+
+        cout << ans << "\n";
     }
 
     return 0;
