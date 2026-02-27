b71a6495a6fe1f6e1761610209e8970f05a3abfc
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-27 20:36:07 +0600

    added

---
+++ CodeForces/A_Eating_Game.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+         {
+            cin >> a[i];
+         }
+         
+    }
+
+    return 0;
+}
