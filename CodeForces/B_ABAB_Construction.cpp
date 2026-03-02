34660270529cf0e694089d6914bb31ca719eecbd
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-02 12:05:48 +0600

    added

--- CodeForces/B_ABAB_Construction.cpp
+++ CodeForces/B_ABAB_Construction.cpp
@@ -18,9 +18,40 @@ int main()
     {
         int n;
         cin >> n;
-        string X;
-        cin >> X;
-        
+        string x;
+        cin >> x;
+
+        int l = 1, r= n;
+        bool isSatisfies = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            char lc = (l % 2 ? 'a' : 'b');
+            char rc = (r % 2 ? 'a' : 'b');
+
+            if (x[i] == '?' || x[i] == lc)
+            {
+                l++;
+            }
+            else if (x[i] == rc)
+            {
+                r--;
+            }
+            else
+            {
+                isSatisfies = false;
+                break;
+            }
+        }
+
+        if (isSatisfies)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
     }
 
     return 0;
