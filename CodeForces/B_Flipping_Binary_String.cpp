242f8a37f73574534a40c4adfd925ab02eb8c847
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-02 11:37:47 +0600

    added

--- CodeForces/B_Flipping_Binary_String.cpp
+++ CodeForces/B_Flipping_Binary_String.cpp
@@ -20,6 +20,44 @@ int main()
         cin >> n;
         string s;
         cin >> s;
+         int cnt1 = 0;
+        for (char c : s)
+        {
+            if (c == '1')
+                cnt1++;
+        }
+
+        int cnt0 = n - cnt1;
+
+        vector<int> ans;
+
+        if (cnt1 % 2 == 0)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] == '1')
+                    ans.push_back(i + 1);
+            }
+        }
+        else if (cnt0 % 2 == 1)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] == '0')
+                    ans.push_back(i + 1);
+            }
+        }
+        else
+        {
+            cout << -1 << '\n';
+            continue;
+        }
+
+        cout << ans.size() << '\n';
+        for (int x : ans)
+            cout << x << " ";
+        cout << '\n';
+    
     }
 
     return 0;
