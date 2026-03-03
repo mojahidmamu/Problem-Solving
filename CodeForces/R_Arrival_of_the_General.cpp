1e9deb7897bdb337ee9a1ceaaa10627549d8d212
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-03 06:38:06 +0600

    added

---
+++ CodeForces/R_Arrival_of_the_General.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    
+
+    return 0;
+}
