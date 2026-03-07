417f9644597c38b1d02ebd5755c45c462af7082e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-07 07:11:19 +0600

    added

--- CodeForces/A_Drawing_Task.cpp
+++ CodeForces/A_Drawing_Task.cpp
@@ -11,20 +11,30 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
     int n, m, q;
     cin >> n >> m >> q;
+    vector<string> grid(n, string(m, '.'));
     while (q--)
     {
         int r1, c1, r2, c2;
         char c;
         cin >> r1 >> c1 >> r2 >> c2 >> c;
-         
-        
-        int arr[n][m];
 
+
+        for (int i = r1 - 1; i <= r2 - 1; i++)
+        {
+            for (int j = c1 - 1; j <= c2 - 1; j++)
+            {
+                grid[i][j] = c;
+            }
+        }
     }
-    
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << grid[i] << endl;
+    }
 
     return 0;
 }
