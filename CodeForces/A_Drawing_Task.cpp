91502e61a3779c0fc4c3343cf9875907ab8c4383
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-07 07:07:05 +0600

    added

--- CodeForces/A_Drawing_Task.cpp
+++ CodeForces/A_Drawing_Task.cpp
@@ -17,17 +17,12 @@ int main()
     while (q--)
     {
         int r1, c1, r2, c2;
-        cin >> r1 >> c1 >> r2 >> c2;
-        int rowDiff = abs(r1 - r2);
-        int colDiff = abs(c1 - c2);
-        if (rowDiff % 2 == colDiff % 2)
-        {
-            cout << "YES" << endl;
-        }
-        else
-        {
-            cout << "NO" << endl;
-        }
+        char c;
+        cin >> r1 >> c1 >> r2 >> c2 >> c;
+         
+        
+        int arr[n][m];
+
     }
     
 
