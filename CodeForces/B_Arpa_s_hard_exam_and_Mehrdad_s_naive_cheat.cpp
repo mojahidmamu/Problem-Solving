f8eff0ee0583e2f0dffef1965d1cd97e51b6f9da
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-07 07:22:37 +0600

    added

--- CodeForces/B_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.cpp
+++ CodeForces/B_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.cpp
@@ -15,6 +15,29 @@ int main()
     ll n;
     cin >> n;
 
-    
+    if (n == 0)
+    {
+        cout << 1 << endl;
+        return 0;
+    }
+
+    ll r = n % 4;
+    if (r == 1)
+    {
+        cout << 8 << endl;
+    }
+    else if(   r == 2)
+    {
+        cout << 4 << endl;
+    }
+    else if(r == 3)
+    {
+        cout << 2 << endl;
+    }
+    else
+    {
+        cout << 6 << endl;
+    }
+
     return 0;
 }
