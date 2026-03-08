9f66a225f414e6ed71438078c18e976229933eaf
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-08 09:43:30 +0600

    added

--- Codeshef/Difficulty_Rating_Order.cpp
+++ Codeshef/Difficulty_Rating_Order.cpp
@@ -1,18 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         int n;
-        cin>>n;
+        cin >> n;
         int d[n];
-        for(int i=0;i<n;i++){
-            cin>>d[i];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> d[i];
         }
         // your code goes here
-        
+        sort(d, d + n);
+        bool flag = true;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (d[i + 1] - d[i] > 1)
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
-
 }
