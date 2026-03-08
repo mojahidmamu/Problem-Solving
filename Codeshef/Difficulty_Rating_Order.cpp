87ec27edd30c44c84ba8dbdc1dda9656ebce0f2b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-08 09:39:14 +0600

    added

---
+++ Codeshef/Difficulty_Rating_Order.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int d[n];
+        for(int i=0;i<n;i++){
+            cin>>d[i];
+        }
+        // your code goes here
+        
+    }
+
+}
