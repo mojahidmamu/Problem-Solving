6dfd623defac3aa0b33d30f9e1d819f61a65ff3b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-08 07:38:29 +0600

    added

---
+++ Codeshef/Running_Comparison.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n], b[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> b[i];
+        }
+        // your code goes here
+    }
+
+    return 0;
+}
