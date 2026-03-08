994375d44e3fee4a2a97b5f0ebb2c061e6d4e504
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-09 05:49:35 +0600

    added

--- CodeForces/B_Subtree_Removal_Game.cpp
+++ CodeForces/B_Subtree_Removal_Game.cpp
@@ -14,13 +14,29 @@ int main()
 
     int n;
     cin >> n;
-    
-    vector<int> a(n);
+
+    vector<int> a(n + 1);
+    vector<int> b(n + 1, 0);
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
+        b[i] = a[i];
     }
-    
+
+    vector<int> leaves;
+    for (int i = 1; i <= n; i++)
+    {
+        if (b[i] == 0)
+        {
+            leaves.push_back(i);
+        }
+    }
+
+    sort(leaves.begin(), leaves.end());
+    int k = leaves.size();
+    int idx = (k + 1) / 2;
+    ;
+    cout << leaves[idx] << endl;
 
     return 0;
 }
