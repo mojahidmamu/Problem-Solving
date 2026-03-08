1bb7b60c0043a706785daa42ffcf314a7471d81f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-08 06:16:20 +0600

    added

--- CodeForces/D_Juicer.cpp
+++ CodeForces/D_Juicer.cpp
@@ -12,12 +12,24 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t;
-    cin >> t;
-    while (t--)
+    int n, b, d;
+    cin >> n >> b >> d;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    int sum = 0, cnt = 0;
+    for (int i = 0; i < n; i++)
     {
-        // write code here...
+        if (arr[i] > b)
+            continue;
+        sum += arr[i];
+        if (sum > d)
+        {
+            cnt++;
+            sum = 0;
+        }
     }
+    cout << cnt << endl;
 
     return 0;
 }
