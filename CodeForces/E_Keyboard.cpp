1a224ca530c27638c10526e8dfdbdcdb94f711cb
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-08 06:33:44 +0600

    added

--- CodeForces/E_Keyboard.cpp
+++ CodeForces/E_Keyboard.cpp
@@ -12,11 +12,51 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t;
-    cin >> t;
-    while (t--)
+    char c;
+    cin >> c;
+
+    string s;
+    cin >> s;
+
+    // Print a line that contains the original message.
+    string keyboard[3] = { "qwertyuiop", "asdfghjkl;", "zxcvbnm,./" };
+    if (c == 'L')
     {
-        // write code here...
+        for (int i = 0; i < s.size(); i++)
+        {
+            char ch = s[i];
+            for (int j = 0; j < 3; j++)
+            {
+                for (int k = 0; k <  3; k++)
+                {
+                    if (keyboard[j][k] == ch)
+                    {
+                        cout << keyboard[j][k + 1];
+                        break;
+                    }
+                }
+            }
+        }
+        cout << endl;
+    }
+    else if (c == 'R')
+    {
+        for (int i = 0; i < s.size(); i++)
+        {
+            char ch = s[i];
+            for (int j = 0; j < 3; j++)
+            {
+                for (int k = 0; k < keyboard[j].size(); k++)
+                {
+                    if (keyboard[j][k] == ch)
+                    {
+                        cout << keyboard[j][k - 1];
+                        break;
+                    }
+                }
+            }
+        }
+        cout << endl;
     }
 
     return 0;
