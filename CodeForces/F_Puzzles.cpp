e60f0aab41707ac583cf8e8352b046a2b26911af
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-08 06:43:58 +0600

    added

---
+++ CodeForces/F_Puzzles.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    int arr[m];
+    for (int i = 0; i < m; i++)
+    {
+        cin >> arr[i];
+    }
+    
+
+    return 0;
+}
