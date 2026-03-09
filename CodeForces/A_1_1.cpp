be0edb2873cf9ef753b252e93138a7a504668f6e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-09 07:25:59 +0600

    added

--- CodeForces/A_1_1.cpp
+++ CodeForces/A_1_1.cpp
@@ -16,7 +16,40 @@ int main()
     cin >> t;
     while (t--)
     {
-        // write code here...
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        int min_ones = 0;
+        int max_ones = 0;
+
+        // max
+        max_ones = count(s.begin(), s.end(), '1');
+        for (int i = 1; i < n - 1; i++)
+        {
+            if (s[i - 1] == '1' && s[i + 1] == '1' && s[i] == '0')
+            {
+                max_ones++;
+            }
+        }
+        // min
+        // int min_ones = 0;
+        for (int i = 0; i < n;)
+        {
+            if (s[i] == '1')
+            {
+                int j = i;
+                while (j < n && s[j] == '1')
+                    j++;
+                min_ones += min(2, j - i); // block contributes 1 if length 1, else 2
+                i = j;
+            }
+            else
+                i++;
+        }
+
+        cout << min_ones << " " << max_ones << endl;
     }
 
     return 0;
