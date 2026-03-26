7b5c3f78f597c86aee7843a728daec9209c7902f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-26 11:01:42 +0600

    added

---
+++ CodeForces/K_Stars_Drawing_Easy_Edition.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> 
+        } 
+    }
+
+    
+    return 0;
+}
