597f75f1ac20f0422d4483d6baba378a216506c5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-26 11:06:14 +0600

    added

--- CodeForces/K_Stars_Drawing_Easy_Edition.cpp
+++ CodeForces/K_Stars_Drawing_Easy_Edition.cpp
@@ -14,14 +14,23 @@ int main()
 
     int n, m;
     cin >> n >> m;
+    char arr[n][m];
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            cin >> 
-        } 
+            cin >> arr[i][j];
+        }
     }
 
-    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j];
+        }
+        cout << endl;
+    }
+
     return 0;
 }
