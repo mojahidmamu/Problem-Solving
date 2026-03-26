9ec4d551aaa1c544cf43a571ca451025773baa4b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-26 11:32:19 +0600

    added

--- CodeForces/L_Announcement.cpp
+++ CodeForces/L_Announcement.cpp
@@ -14,10 +14,31 @@ int main()
 
     int n;
     cin >> n;
-    int arr[n];
+    vector<int> arr(n);
+    unordered_map<int, int> freq;
+
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
+        freq[arr[i]]++;
+    }
+
+    int replace = 0;
+    for (auto &p : freq)
+    {
+        if (p.second > 1)
+        {
+            replace += (p.second - 1);
+        }
+    }
+
+    if (replace == 0)
+    {
+        cout << -1 << endl;
+    }
+    else
+    {
+        cout << replace << endl;
     }
 
     return 0;
