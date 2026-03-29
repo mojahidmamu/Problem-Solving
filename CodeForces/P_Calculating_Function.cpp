0727397e3634139ea03d1cf1de32e1ef910cd5c7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-29 19:19:35 +0600

    added

--- CodeForces/P_Calculating_Function.cpp
+++ CodeForces/P_Calculating_Function.cpp
@@ -14,7 +14,17 @@ int main()
 
     ll n;
     cin >> n;
-    
+
+    ll ans;
+    if (n % 2 == 0)
+    {
+        ans = n / 2;
+    }
+    else
+    {
+        ans = -(n + 1) / 2;
+    }
+    cout << ans << endl;
 
     return 0;
 }
