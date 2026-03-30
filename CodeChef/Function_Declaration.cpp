375d47c34bae052d834bde773769d2d345ffe64d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-30 18:45:30 +0600

    added

--- CodeChef/Function_Declaration.cpp
+++ CodeChef/Function_Declaration.cpp
@@ -1,25 +1,44 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-#define ll long long
-#define endl '\n'
-#define all(x) (x).begin(), (x).end()
+// #define ll long long
+// #define endl '\n'
+// #define all(x) (x).begin(), (x).end()
 
-// Author: Abdullah all Mojahid
+// // Author: Abdullah all Mojahid
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
 
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n;
+//         cin >> n;
+
+//         class Solution {
+// public:
+//     int dfs(TreeNode * node, int &ans) {
+//         if(node == NULL) return 0;
         
-    }
+//         int left = max(0, dfs(node->left, ans));
+//         int right = max(0, dfs(node->right, ans));
+        
+//         ans = max(ans, left + right + node->val);
+        
+//         return node->val + max(left, right);
+//     }
 
-    return 0;
-}
+//     int maxPathSum(TreeNode* root) {
+//         int ans = INT_MIN;
+//         dfs(root, ans);
+//         return ans;
+//     }
+// };
+//     }
+
+//     return 0;
+// }
