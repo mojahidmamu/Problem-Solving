afd66effdeda5bfa88c93e433df36508de1bb6e0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-30 12:40:54 +0600

    added

--- CodeForces/S_Mahmoud_and_a_Triangle.cpp
+++ CodeForces/S_Mahmoud_and_a_Triangle.cpp
@@ -14,16 +14,17 @@ int main()
 
     int n;
     cin >> n;
-    ll arr[n];
+    vector<ll> arr(n);
     bool isAccept = false;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
+    sort(arr.begin(), arr.end());
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] + arr[i + 1] >= arr[i + 2])
+        if (arr[i] + arr[i + 1] > arr[i + 2])
         {
             isAccept = true;
         }
