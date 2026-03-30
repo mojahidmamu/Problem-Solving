75b5daf19a08291dc5b37e41653cb2d321e1aad6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-30 12:39:35 +0600

    added

--- CodeForces/S_Mahmoud_and_a_Triangle.cpp
+++ CodeForces/S_Mahmoud_and_a_Triangle.cpp
@@ -15,10 +15,18 @@ int main()
     int n;
     cin >> n;
     ll arr[n];
-    bool isAccept = true;
+    bool isAccept = false;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] + arr[i + 1] >= arr[i + 2])
+        {
+            isAccept = true;
+        }
     }
 
     if (isAccept == true)
