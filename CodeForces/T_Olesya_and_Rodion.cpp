5ec6c1af1d3e44d14e8ba139d0d9e18bb9064a42
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-31 09:16:38 +0600

    added

--- CodeForces/T_Olesya_and_Rodion.cpp
+++ CodeForces/T_Olesya_and_Rodion.cpp
@@ -12,9 +12,31 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n,  t;
+    int n, t;
     cin >> n >> t;
-    
+
+    if (t == 10)
+    {
+        if (n == 1)
+        {
+            cout << -1 << endl;
+        }
+        else
+        {
+            cout << 1;
+            for (int i = 0; i < n - 1; i++)
+            {
+                cout << 0;
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << t;
+        }
+    }
 
     return 0;
 }
