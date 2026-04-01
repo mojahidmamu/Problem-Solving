9febbd4019b41f40cdc546692ec2fae101c4fb8a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-01 20:45:24 +0600

    added

--- CodeChef/Vacation_Clothes.cpp
+++ CodeChef/Vacation_Clothes.cpp
@@ -1,10 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	// your code goes here
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
     cin >> n;
-    
-	
+    cout << min(n, 7) << endl;
+
+    return 0;
 }
