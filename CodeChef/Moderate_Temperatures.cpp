c2956ba1ba2f931aa3b29d11008d195e42b7509e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-01 20:46:33 +0600

    added

---
+++ CodeChef/Moderate_Temperatures.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        
+        
+    }
+
+    return 0;
+}
