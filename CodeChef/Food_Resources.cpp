7693b5bfb141989435add5983741365f1946cbef
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-01 20:55:52 +0600

    added

---
+++ CodeChef/Food_Resources.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+    }
+    
+    
+
+    return 0;
+}
