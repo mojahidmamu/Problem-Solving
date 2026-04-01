9f08baff5e5203f41018354c2303f1cc4f10ee61
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-01 21:13:41 +0600

    added

---
+++ CodeForces/B_Are_You_Smiling.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s = "HAPPY";
+    
+    for (char c : s) {
+        cout << (int)c;
+    }
+    
+
+    return 0;
+}
