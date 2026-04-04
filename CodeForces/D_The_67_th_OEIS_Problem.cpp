096ca1ee77cdafe40ee87194bc5931bf8e077e32
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-04 21:34:21 +0600

    added

--- CodeForces/D_The_67_th_OEIS_Problem.cpp
+++ CodeForces/D_The_67_th_OEIS_Problem.cpp
@@ -1,34 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define endl '\n'
-#define all(x) (x).begin(), (x).end()
-
-// Author: Abdullah all Mojahid
-
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
-    while (t--)
-    {
+
+    while (t--) {
         int n;
         cin >> n;
 
-        vector<long long> a(n);
-
-        a[0] = 1;
-        for (int i = 2; i <= n; i++)
-        {
-            a[i - 1] = 1LL * (i + 1) * (i + 2);
+        for (int i = 1; i <= n; i++) {
+            cout << 1LL * i * (i + 1) << " ";
         }
-
-        for (auto x : a)
-            cout << x << " ";
         cout << '\n';
     }
 
