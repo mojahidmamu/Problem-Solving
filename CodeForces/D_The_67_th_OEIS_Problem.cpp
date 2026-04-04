47b1198f854e2b0b4062fc18a9aed1f0d974a9c7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-04 20:48:11 +0600

    added

--- CodeForces/D_The_67_th_OEIS_Problem.cpp
+++ CodeForces/D_The_67_th_OEIS_Problem.cpp
@@ -18,13 +18,6 @@ int main()
     {
         int n;
         cin >> n;
-        
-        for (int  i = 1; i <= n; i++)
-        {
-            cout << i << " " << n + i << " " << 2 * n + i << " ";
-        }
-        cout << '\n';
-        
     }
 
     return 0;
