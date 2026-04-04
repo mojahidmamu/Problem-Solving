d5738e438931c9cf14f51bfb204294bbaf00358e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-04 20:36:51 +0600

    added

---
+++ CodeForces/A_The_67_th_Integer_Problem.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+    }
+
+    return 0;
+}
