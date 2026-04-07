80e61f74e1f2c5dd5357bbb1c9de7f90c8ec1b7b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-07 21:36:58 +0600

    added

--- CodeForces/D_Flip_the_Bit_Hard_Version.cpp
+++ CodeForces/D_Flip_the_Bit_Hard_Version.cpp
@@ -16,7 +16,24 @@ int main()
     cin >> t;
     while (t--)
     {
-        // write code here...
+        int n, k;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == 0)
+            {
+                ans++;
+                i += k;
+            }
+        }
+        cout << ans << endl;
     }
 
     return 0;
