ce77f1740df4db337a4e86f366005489602453aa
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-07 21:25:23 +0600

    added

--- CodeForces/B_Flip_the_Bit_Easy_Version.cpp
+++ CodeForces/B_Flip_the_Bit_Easy_Version.cpp
@@ -7,37 +7,41 @@ int main() {
     
     int t;
     cin >> t;
-    while (t--) {
+    for(int test = 0; test < t; test++) {
         int n, k;
         cin >> n >> k;
+        // k==1 always
         vector<int> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
+        for(int i=0; i<n; i++) {
+            cin >> a[i];
+        }
         int p;
-        cin >> p;
-        p--;
+        cin >> p;  // 1-based
+        p--;       // make 0-based
         int x = a[p];
         
-        int left = 0, right = 0;
-        int flip = 0;
-        for (int i = p-1; i >= 0; i--) {
-            if (a[i] != x) {
-                if (flip == 0) left++;
-                flip ^= 1;
-            } else {
-                flip = 0;
-            }
-        }
-        flip = 0;
-        for (int i = p+1; i < n; i++) {
-            if (a[i] != x) {
-                if (flip == 0) right++;
-                flip ^= 1;
-            } else {
-                flip = 0;
+        // count left runs: 0 to p-1
+        int left_runs = 0;
+        for(int i=0; i<p; i++) {
+            if(a[i] != x) {
+                if(i==0 || a[i-1] == x) {
+                    left_runs++;
+                }
             }
         }
         
-        cout << left + right << "\n";
+        // count right runs: p+1 to n-1
+        int right_runs = 0;
+        for(int i=p+1; i<n; i++) {
+            if(a[i] != x) {
+                if(i==p+1 || a[i-1] == x) {
+                    right_runs++;
+                }
+            }
+        }
+        
+        int ans = 2 * max(left_runs, right_runs);
+        cout << ans << '\n';
     }
     return 0;
-}     
+}
