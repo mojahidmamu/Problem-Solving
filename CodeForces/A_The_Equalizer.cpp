02aff597d472eec36e174c487277d8de7778735b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-07 20:36:26 +0600

    added

---
+++ CodeForces/A_The_Equalizer.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+    }
+
+    return 0;
+}
