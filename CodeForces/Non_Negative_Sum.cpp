c444b0bc887086eda053a5f2f6749aa4a6f4299c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-15 20:43:26 +0600

    added

---
+++ CodeForces/Non_Negative_Sum.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+        }
+        
+    }
+
+    return 0;
+}
