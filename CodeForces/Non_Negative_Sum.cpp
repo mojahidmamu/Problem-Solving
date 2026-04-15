1f60be4b0eae3d70e6e236c34a2035e89ca7d804
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-15 20:50:00 +0600

    added

--- CodeForces/Non_Negative_Sum.cpp
+++ CodeForces/Non_Negative_Sum.cpp
@@ -10,12 +10,28 @@ int main()
     {
         int n;
         cin >> n;
+        vector<int> arr(n);
         for (int i = 0; i < n; i++)
         {
-            int x;
-            cin >> x;
+            cin >> arr[i];
         }
-        
+        sort(arr.rbegin(), arr.rend());
+
+        int sum = 0, count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+            if (sum >= 0)
+            {
+                count++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        cout << count << endl;
     }
 
     return 0;
