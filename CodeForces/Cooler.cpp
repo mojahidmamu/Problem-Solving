2d5c801f05c8b643f43a3619a7aa70ad7337846b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-15 20:40:25 +0600

    added

--- CodeForces/Cooler.cpp
+++ CodeForces/Cooler.cpp
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	// your code goes here
-    int n, m;
-    cin >> n >> m;
-    
+int main()
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        int time = (n * (n + 1)) / 2 - (m * (m + 1)) / 2;
+        cout << time << endl;
+    }
 }
