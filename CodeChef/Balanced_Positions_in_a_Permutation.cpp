d61424a58134354a6d7cc2b15c7ad38d80bd0fc3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-22 20:43:54 +0600

    added

--- CodeChef/Balanced_Positions_in_a_Permutation.cpp
+++ CodeChef/Balanced_Positions_in_a_Permutation.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main()
 {
-   
 
     int t;
     cin >> t;
@@ -18,9 +16,29 @@ int main()
             cin >> arr[i];
         }
 
-        
-        
-        
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int l = 0, r = 0;
+
+            for (int j = 0; j < i; j++)
+            {
+                if (arr[j] < arr[i])
+                    l++;
+            }
+
+            
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[j] > arr[i])
+                    r++;
+            }
+
+            if (l == r)
+                count++;
+        }
+
+        cout << count << endl;
     }
 
     return 0;
