a4ea4881ba5d26afe3da25a49b4dbc9ed496dc57
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-30 20:46:18 +0600

    added

---
+++ CodeForces/C_Snowfall.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        
+    }
+
+    return 0;
+}
