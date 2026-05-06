f1986f9dd8640869d806975f2b03fbd711327265
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-05-06 20:33:57 +0600

    added

--- CodeChef/Election.cpp
+++ CodeChef/Election.cpp
@@ -1,10 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	// your code goes here
+int main()
+{
+    // your code goes here
     int n, k;
     cin >> n >> k;
-    
+
+    int majority = (n / 2) + 1;
+    int additional = majority - k;
+
+    // If already has majority
+    if (additional < 0)
+    {
+        additional = 0;
+    }
+
+    cout << additional << endl;
     return 0;
 }
