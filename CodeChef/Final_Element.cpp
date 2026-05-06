09c1c141c2838ce48d1efaac7568ea3284de4b02
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-05-06 20:48:01 +0600

    added

--- CodeChef/Final_Element.cpp
+++ CodeChef/Final_Element.cpp
@@ -10,13 +10,22 @@ int main()
         int n;
         cin >> n;
 
+        long long arr[n];
         for (int i = 0; i < n; i++)
         {
-            int a;
-            cin >> a;
-
-            
+            cin >> arr[i];
         }
+
+        int result = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if ((i & (n - 1)) == i)
+            {
+                result ^= arr[i];
+            }
+        }
+
+        cout << result << endl;
     }
 
     return 0;
