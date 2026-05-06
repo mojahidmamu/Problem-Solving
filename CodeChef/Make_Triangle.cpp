6aaf7557d658fcf2f436fa9011e0651f9b7c2145
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-05-06 20:36:30 +0600

    added

--- CodeChef/Make_Triangle.cpp
+++ CodeChef/Make_Triangle.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main()
 {
 
@@ -9,9 +8,29 @@ int main()
     cin >> t;
     while (t--)
     {
-        int x,y, z;
+        int x, y, z;
         cin >> x >> y >> z;
-        
+
+        int a = x, b = y, c = z;
+
+        // sort a, b, c
+        if (a > b)
+            swap(a, b);
+        if (b > c)
+            swap(b, c);
+        if (a > b)
+            swap(a, b);
+
+        // check triangle condition
+        if (a + b > c)
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            int moves = (c - (a + b)) + 1;
+            cout << moves << endl;
+        }
     }
 
     return 0;
