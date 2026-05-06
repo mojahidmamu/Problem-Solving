2a6bea544530e4c365d3a51a48a46cf093badfa3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-05-06 20:41:55 +0600

    added

--- CodeChef/Final_Element.cpp
+++ CodeChef/Final_Element.cpp
@@ -7,7 +7,14 @@ int main()
     cin >> t;
     while (t--)
     {
-         
+        int n;
+        cin >> n;
+
+        for (int i = 0; i < n; i++)
+        {
+            int a;
+            cin >> a;
+        }
     }
 
     return 0;
