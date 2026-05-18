2eaa708170ea04cd1fe7358f5abb425ea8390d4d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-05-18 20:50:12 +0600

    added

---
+++ CodeForces/C_Arrange_the_Numbers_in_a_Circle.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<ll> arr(n);
+        for (int i = 0; i < count; i++)
+        {
+            /* code */
+        }
+        
+    }
+
+    return 0;
+}
