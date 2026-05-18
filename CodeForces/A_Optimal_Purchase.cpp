14b9aa2b62002334dd32f893af966d8309413c5d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-05-18 20:45:34 +0600

    added

--- CodeForces/A_Optimal_Purchase.cpp
+++ CodeForces/A_Optimal_Purchase.cpp
@@ -16,15 +16,20 @@ int main()
     cin >> t;
     while (t--)
     {
-        int n, a, b;
+        ll n, a, b;
         cin >> n >> a >> b;
-        long long ans = n * a;
-        long long m = n / 3;
-        long long r = n % 3;
-        long long c1 = m * b + r * a;
-        long long c2 = (m + 1) * b;
-        ans = min(ans, c1);
-        ans = min(ans, c2);
+        long long full_groups = n / 3;
+        long long remainder = n % 3;
+
+        // Option 1: all individual keys
+        long long cost_all_individual = n * a;
+
+        // Option 2: use as many group keys as possible, then handle remainder
+        long long cost_group_mix = full_groups * b + min(remainder * a, b);
+
+        // Answer is the minimum of the two
+        long long ans = min(cost_all_individual, cost_group_mix);
+
         cout << ans << endl;
     }
 
