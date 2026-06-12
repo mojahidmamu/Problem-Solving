6d7fb751ae9fda14e8f4a022a1f04167689cc059
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-06-12 20:42:54 +0600

    added

---
+++ CodeForces/B_Tatar_TV_Show.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        string s;
+        cin >> s;
+
+        bool can = true;
+        for (int i = 0; i < k; i++)
+        {
+            int ones = 0;
+            for (int j = 0; j < n; j += k)
+            {
+                if (s[j] == '1')
+                {
+                    ones++;
+                }
+            }
+
+            if (ones % 2 == 0)
+            {
+                can = false;
+                break;
+            }
+        }
+
+        if (can == true)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
