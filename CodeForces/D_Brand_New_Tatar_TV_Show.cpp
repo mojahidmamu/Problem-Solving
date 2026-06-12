5144efac67f42aa93ab9a05c416d511dff45fc17
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-06-12 21:22:08 +0600

    added

--- CodeForces/D_Brand_New_Tatar_TV_Show.cpp
+++ CodeForces/D_Brand_New_Tatar_TV_Show.cpp
@@ -18,11 +18,54 @@ int main()
     {
         int n, k;
         cin >> n >> k;
-        vector<int> a(n);
-        for (int &x : a)
+
+        vector<int> cnt(n + 2, 0);
+
+        for (int i = 0; i < n; i++)
+        {
+            int x;
             cin >> x;
-        
-        
+            cnt[x]++;
+        }
+
+        vector<char> win(n + 2, 0);
+        deque<int> dq; // losing positions with cnt[pos] > 0
+
+        for (int i = n; i >= 1; i--)
+        {
+            while (!dq.empty() && dq.front() > i + k)
+            {
+                dq.pop_front();
+            }
+
+            bool hasLosingReachable = !dq.empty();
+
+            if (hasLosingReachable)
+            {
+                win[i] = 1;
+            }
+            else
+            {
+                win[i] = (cnt[i] % 2 == 0);
+            }
+
+            if (cnt[i] > 0 && !win[i])
+            {
+                dq.push_back(i);
+            }
+        }
+
+        bool answer = false;
+        for (int v = 1; v <= n; v++)
+        {
+            if (cnt[v] > 0 && win[v])
+            {
+                answer = true;
+                break;
+            }
+        }
+
+        cout << (answer ? "YES" : "NO") << '\n';
     }
 
     return 0;
