4f86dd9d6b00c08fd0c5ee45f3c2afd25ae83d92
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-06-30 20:50:43 +0600

    added

--- CodeForces/B_Good_times_Good_times.cpp
+++ CodeForces/B_Good_times_Good_times.cpp
@@ -7,6 +7,20 @@ using namespace std;
 
 // Author: Abdullah all Mojahid
 
+bool isGood(long long n)
+{
+    int mask = 0;
+    while (n > 0)
+    {
+        int d = n % 10;
+        mask |= (1 << d);
+        n /= 10;
+        if (__builtin_popcount(mask) > 2)
+            return false;
+    }
+    return true;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -18,6 +32,18 @@ int main()
     {
         int x;
         cin >> x;
+
+        for (long long y = 2;; y++)
+        {
+            if (!isGood(y))
+                continue;
+            long long prod = x * y;
+            if (isGood(prod))
+            {
+                printf("%lld\n", y);
+                break;
+            }
+        }
     }
 
     return 0;
