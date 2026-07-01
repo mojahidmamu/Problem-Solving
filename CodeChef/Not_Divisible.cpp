f4d287160f1fbb0356d3781d633d0e3dd99dda49
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-07-01 20:53:48 +0600

    added

--- CodeChef/Not_Divisible.cpp
+++ CodeChef/Not_Divisible.cpp
@@ -1,61 +1,58 @@
-#include <bits/stdc++.h>
-using namespace std;
+    #include <bits/stdc++.h>
+    using namespace std;
 
-#define ll long long
-#define endl '\n'
-#define all(x) (x).begin(), (x).end()
+    
 
-// Author: Abdullah all Mojahid
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while (t--)
+    int main()
     {
-        int N;
-        cin >> N;
-        vector<vector<int>> pos(N + 1);
+        
 
-        for (int i = 1; i <= N; i++)
+        int t;
+        cin >> t;
+        while (t--)
         {
-            int x;
-            cin >> x;
-            pos[N + 1 - x].push_back(i);
+            int N;
+            cin >> N;
+            vector<vector<int>> pos(N + 1);
+
+            for (int i = 1; i <= N; i++)
+            {
+                int x;
+                cin >> x;
+                pos[N + 1 - x].push_back(i);
+            }
+
+            int mx = 0;
+            for (int v = 1; v <= N; v++) {
+                mx = max(mx, (int)pos[v].size());
+
+            }
+
+            if (mx > N / 2)
+            {
+                cout << -1 << '\n';
+                continue;
+            }
+
+            vector<pair<int, int>> a;
+            for (int v = 1; v <= N; v++) 
+                for (int idx : pos[v])
+                    a.push_back({v, idx});
+
+            sort(a.begin(), a.end());
+
+            vector<int> ans(N + 1);
+
+            int m = a.size();
+            for (int i = 0; i < m; i++)
+            {
+                ans[a[i].second] = a[(i + mx) % m].first;
+            }
+
+            for (int i = 1; i <= N; i++)
+                cout << ans[i] << " ";
+            cout << '\n';
         }
 
-        int mx = 0;
-        for (int v = 1; v <= N; v++)
-            mx = max(mx, (int)pos[v].size());
-
-        if (mx > N / 2)
-        {
-            cout << -1 << '\n';
-            continue;
-        }
-
-        vector<pair<int, int>> a;
-        for (int v = 1; v <= N; v++)
-            for (int idx : pos[v])
-                a.push_back({v, idx});
-
-        sort(a.begin(), a.end());
-
-        vector<int> ans(N + 1);
-
-        int m = a.size();
-        for (int i = 0; i < m; i++)
-        {
-            ans[a[i].second] = a[(i + mx) % m].first;
-        }
-
-        for (int i = 1; i <= N; i++)
-            cout << ans[i] << " ";
-        cout << '\n';
+        return 0;
     }
-
-    return 0;
-}
