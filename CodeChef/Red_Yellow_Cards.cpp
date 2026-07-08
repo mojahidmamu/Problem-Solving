e0ca4572a334f7a813aa9f23edbb88e84a55f216
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-07-08 20:37:29 +0600

    added

---
+++ CodeChef/Red_Yellow_Cards.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int R, Y;
+        cin >> R >> Y;
+
+        cout << R + (Y / 2) << '\n';
+    }
+
+    return 0;
+}
