48ae6d8fd69dc56f23ac2c1b3ee7bd9858d07990
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-07-08 20:44:28 +0600

    added

---
+++ CodeChef/Waiting_Hard.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<long long> A(n + 1);
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> A[i];
+        }
+    }
+
+    return 0;
+}
