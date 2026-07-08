bb3575652e428ac1381ebf1835353e2dafa99217
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-07-08 20:33:45 +0600

    added

--- CodeChef/Plant.cpp
+++ CodeChef/Plant.cpp
@@ -23,7 +23,12 @@ int main()
         {
             cin >> a[i];
         }
-        
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            ans = max(ans, min(a[i], a[i + 1]));
+        }
+        cout << ans << endl;
     }
 
     return 0;
