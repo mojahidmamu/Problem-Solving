27000b8e25f0037ec86af0260a73b46419ded300
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-07-13 19:50:52 +0600

    added

--- CodeChef/Bead_Necklaces.cpp
+++ CodeChef/Bead_Necklaces.cpp
@@ -12,15 +12,47 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+    int N;
+    cin >> N;
+    vector<long long> A(N);
+    for (int i = 0; i < N; i++)
     {
-        cin >> arr[i];
+        cin >> A[i];
     }
-    
-    
+
+    vector<vector<bool>> pal(N, vector<bool>(N, false));
+
+    for (int i = 0; i < N; i++)
+        pal[i][i] = true;
+
+    for (int i = 0; i + 1 < N; i++)
+        pal[i][i + 1] = (A[i] == A[i + 1]);
+
+    for (int len = 3; len <= N; len++)
+    {
+        for (int l = 0; l + len - 1 < N; l++)
+        {
+            int r = l + len - 1;
+            pal[l][r] = (A[l] == A[r]) && pal[l + 1][r - 1];
+        }
+    }
+
+    const int INF = 1e9;
+    vector<int> dp(N + 1, INF);
+    dp[0] = 0;
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (pal[j][i - 1])
+            {
+                dp[i] = min(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    cout << dp[N] << '\n';
 
     return 0;
 }
