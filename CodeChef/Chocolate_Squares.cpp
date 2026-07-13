5ae2eabb4a3b80ddd71aa25a6f6b83f1fdff6daf
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-07-13 19:42:16 +0600

    added

---
+++ CodeChef/Chocolate_Squares.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int L, B;
+        cin >> L >> B;
+
+        cout << __gcd(L, B) << '\n';
+    }
+
+    return 0;
+}
