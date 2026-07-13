ffe8feeba42778e5eb8e35eed04b4db6029bbeb0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-07-13 19:39:31 +0600

    added

---
+++ CodeChef/Monthly_Earnings.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Author: Abdullah all Mojahid
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int S, B, R;
+    cin >> S >> B >> R;
+
+    cout << S + (B * R) << '\n';
+
+    return 0;
+}
